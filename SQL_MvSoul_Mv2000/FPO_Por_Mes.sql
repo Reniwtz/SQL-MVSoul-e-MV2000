7f2732424510a3c449cc87a9ac3966658249a451
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2023-03-07 17:51:00 -0300

    Update FPO_Por_Mes.sql

--- SQL_MvSoul_Mv2000/FPO_Por_Mes.sql
+++ SQL_MvSoul_Mv2000/FPO_Por_Mes.sql
@@ -1,12 +1,44 @@
-        select t1.cd_procedimento Cod_Procedimento, t3.ds_procedimento Descricao, t2.qt_fisico Acordado, count(t1.cd_procedimento) Qt_Lancada
-            from eve_siasus t1, teto_orcamentario_proced_sus t2, procedimento_sus t3
-        where
-            t1.cd_procedimento in (select cd_procedimento from procedimento_sus) and
-            t1.cd_fat_sia in (select cd_fat_sia from fat_sia where to_char(dt_periodo_inicial, 'MM/YYYY') = '02/2023') and
-            to_char(t1.dt_eve_siasus, 'mm/yyyy') = '02/2023' and
-            t2.cd_fat_sia in (select cd_fat_sia from fat_sia where to_char(dt_periodo_inicial, 'mm/yyyy') = '02/2023') and
-            t1.cd_procedimento = t2.cd_procedimento and
-            t1.cd_procedimento = t3.cd_procedimento and
-            t1.qt_lancada <> 0
-        group by t1.cd_procedimento, t3.ds_procedimento, t2.qt_fisico
-        order by t1.cd_procedimento
+SELECT
+    t1.cd_procedimento                                     AS código,
+    t3.ds_procedimento                                     AS descrição,
+    t2.qt_fisico                                           AS orçado,
+    COUNT(t1.cd_procedimento)                              AS lançada,
+    ( t2.qt_fisico - COUNT(t1.cd_procedimento) )           AS resta,
+    ( ( COUNT(t1.cd_procedimento) / t2.qt_fisico ) * 100)  AS porcentagem
+FROM
+    eve_siasus                   t1,
+    teto_orcamentario_proced_sus t2,
+    procedimento_sus             t3
+WHERE
+    t1.cd_procedimento IN (
+        SELECT
+            cd_procedimento
+        FROM
+            procedimento_sus
+    )
+    AND t1.cd_fat_sia IN (
+        SELECT
+            cd_fat_sia
+        FROM
+            fat_sia
+        WHERE
+            to_char(dt_periodo_inicial, 'MM/YYYY') = '03/2023'
+    )
+    AND to_char(t1.dt_eve_siasus, 'mm/yyyy') = '03/2023'
+    AND t2.cd_fat_sia IN (
+        SELECT
+            cd_fat_sia
+        FROM
+            fat_sia
+        WHERE
+            to_char(dt_periodo_inicial, 'mm/yyyy') = '03/2023'
+    )
+    AND t1.cd_procedimento = t2.cd_procedimento
+    AND t1.cd_procedimento = t3.cd_procedimento
+    AND t1.qt_lancada <> 0
+GROUP BY
+    t1.cd_procedimento,
+    t3.ds_procedimento,
+    t2.qt_fisico
+ORDER BY
+    t1.cd_procedimento
