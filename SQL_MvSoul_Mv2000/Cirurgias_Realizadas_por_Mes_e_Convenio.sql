e702e2890cfd642d12cd9e8583576b7652311b3f
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2023-04-26 11:03:57 -0300

    Update Cirurgias_Realizadas_por_Mes_e_Convenio.sql

--- SQL_MvSoul_Mv2000/Cirurgias_Realizadas_por_Mes_e_Convenio.sql
+++ SQL_MvSoul_Mv2000/Cirurgias_Realizadas_por_Mes_e_Convenio.sql
@@ -1,21 +1,35 @@
-SELECT
-    decode(cirurgia_aviso.cd_convenio , '1', 'SUS - INT', '2', 'SUS - AMB', '16', 'Particular', 'P.Saude') Convenio, substr(to_CHAR(aviso_cirurgia.dt_realizacao, 'MONTH'),0,3) Mes_Atend, COUNT ( cirurgia_aviso.cd_convenio ) CONT_CONV
-FROM
+SELECT 
+    CASE 
+        WHEN cirurgia_aviso.cd_convenio IN ('1', '2') THEN 'SUS'
+        WHEN cirurgia_aviso.cd_convenio = '16' THEN 'Particular'
+        ELSE 'P.Saude'
+    END AS Convenio,
+    SUBSTR(TO_CHAR(aviso_cirurgia.dt_realizacao, 'MONTH'),0,3) AS Mes_Atend,
+    COUNT(cirurgia_aviso.cd_convenio) AS CONT_CONV
+    --SUM(CASE WHEN cirurgia_aviso.cd_convenio IN ('1', '2') THEN 1 ELSE 0 END) AS Saídas_SUS,
+    --SUM(CASE WHEN cirurgia_aviso.cd_convenio = '16' THEN 1 ELSE 0 END) AS Saídas_Particular,
+    --SUM(CASE WHEN cirurgia_aviso.cd_convenio NOT IN ('1', '2', '16') THEN 1 ELSE 0 END) AS Saídas_Convenio
+FROM 
     aviso_cirurgia aviso_cirurgia,
     cirurgia_aviso cirurgia_aviso,
-    cirurgia       cirurgia,
-    convenio       convenio,
+    cirurgia cirurgia,
+    convenio convenio,
     empresa_convenio
-WHERE
-        empresa_convenio.cd_convenio = convenio.cd_convenio
+WHERE 
+    empresa_convenio.cd_convenio = convenio.cd_convenio
     AND aviso_cirurgia.tp_situacao = 'R'
     AND aviso_cirurgia.cd_aviso_cirurgia = cirurgia_aviso.cd_aviso_cirurgia
     AND cirurgia_aviso.cd_cirurgia = cirurgia.cd_cirurgia
     AND convenio.cd_convenio = '1'
     AND aviso_cirurgia.cd_cen_cir = '1'
-    AND aviso_cirurgia.dt_realizacao BETWEEN ( '01/01/2023' ) AND ( '31/03/2023' )
+    AND aviso_cirurgia.dt_realizacao BETWEEN ('01/01/2023') AND ('31/03/2023')
 GROUP BY 
-    cirurgia_aviso.cd_convenio , substr(to_CHAR(aviso_cirurgia.dt_realizacao, 'MONTH'),0,3), convenio.nm_convenio
-order by 
-    mes_atend;
-
+    CASE 
+        WHEN cirurgia_aviso.cd_convenio IN ('1', '2') THEN 'SUS'
+        WHEN cirurgia_aviso.cd_convenio = '16' THEN 'Particular'
+        ELSE 'P.Saude'
+    END,
+    SUBSTR(TO_CHAR(aviso_cirurgia.dt_realizacao, 'MONTH'),0,3),
+    convenio.nm_convenio   
+ORDER BY 
+    Mes_Atend;
