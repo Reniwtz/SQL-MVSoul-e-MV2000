a9ae242ae03cc0d57ae65ade93a3b6acdc0773d8
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2023-05-31 15:03:10 -0300

    Update fpo_reformulado.sql

--- SQL_MvSoul_Mv2000/fpo_reformulado.sql
+++ SQL_MvSoul_Mv2000/fpo_reformulado.sql
@@ -51,4 +51,30 @@ GROUP BY
 ORDER BY
     teto_orcamentario_proced_sus.cd_procedimento,
     eve_siasus.cd_procedimento;
+  
+  
+-------------------------------------------------------------------------
+
+SELECT
+    atendime.cd_procedimento AS Procedimento,
+    procedimento_sus.ds_procedimento AS descricao,
+    teto_orcamentario_proced_sus.qt_fisico AS acordado,
+    count(atendime.cd_procedimento) AS Quantidade_Feita,
+    teto_orcamentario_proced_sus.vl_orcamento AS valor
+FROM
+    atendime atendime
+    INNER JOIN teto_orcamentario_proced_sus ON atendime.cd_procedimento = teto_orcamentario_proced_sus.cd_procedimento
+    INNER JOIN procedimento_sus ON teto_orcamentario_proced_sus.cd_procedimento = procedimento_sus.cd_procedimento
+    INNER JOIN fat_sia ON teto_orcamentario_proced_sus.cd_fat_sia = fat_sia.cd_fat_sia
+WHERE
+        atendime.dt_atendimento BETWEEN ( '01/04/2023' ) AND ( '30/04/2023' )
+    AND to_char(fat_sia.dt_periodo_inicial, 'MM/YYYY') = '04/2023'
+    AND atendime.sn_atendimento_apac LIKE 'S'
+GROUP BY
+    atendime.cd_procedimento,
+    procedimento_sus.ds_procedimento,
+    teto_orcamentario_proced_sus.qt_fisico, 
+    teto_orcamentario_proced_sus.vl_orcamento
+ORDER BY
+    atendime.cd_procedimento;
     
