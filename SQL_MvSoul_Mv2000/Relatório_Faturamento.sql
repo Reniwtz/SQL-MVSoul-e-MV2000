a6454404f43e826203904d06477260a6219bc6de
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2023-06-19 16:51:55 -0300

    Update Relatório_Faturamento.sql

--- SQL_MvSoul_Mv2000/Relatório_Faturamento.sql
+++ SQL_MvSoul_Mv2000/Relatório_Faturamento.sql
@@ -1,28 +1,67 @@
 SELECT
+    ori_ate.ds_ori_ate AS Origem,
     atendime.cd_atendimento AS Atendimento,
-    paciente.nm_paciente AS Nome_do_Paciente,
+    paciente.nm_paciente AS Paciente,
     atendime.cd_paciente AS CAD_do_Paciente,
     paciente.nr_cpf AS CPF,
-    ori_ate.ds_ori_ate AS Origem,
     atendime.dt_atendimento AS Data_do_Atendimento,
+    atendime.dt_alta AS Data_da_Alta,
     convenio.nm_convenio AS Convênio,
     prestador.nm_prestador AS Prestador,
     atendime.cd_pro_int AS Procedimento,
-    reg_fat.cd_remessa AS Remessa,
-    reg_fat.vl_total_conta AS Valor_da_Conta,
-    remessa_fatura.ds_lote_tiss AS Lote,
-    tiss_mensagem.nr_protocolo_retorno AS Protocolo,
-    to_char(fatura.dt_competencia, 'mm-yyyy') AS Competência
+    (SELECT
+        SUM(itreg_amb.vl_total_conta)
+    FROM
+            atendime atendime
+        INNER JOIN itreg_amb ON atendime.cd_atendimento = itreg_amb.cd_atendimento
+    WHERE
+            atendime.dt_atendimento BETWEEN ( '01/01/2023' ) AND ( '31/01/2023')
+        AND atendime.cd_convenio LIKE '3'
+        AND atendime.CD_ATENDIMENTO LIKE '3581347'
+        AND (cd_lancamento like '1' 
+        OR cd_lancamento LIKE '2'
+        OR cd_lancamento LIKE '3'
+        OR cd_lancamento LIKE '4'
+        OR cd_lancamento LIKE '5'
+        OR cd_lancamento LIKE '6'
+        OR cd_lancamento LIKE '7'
+        OR cd_lancamento LIKE '8') ) AS VRL_HNL,
+    (SELECT
+        SUM(itreg_amb.vl_total_conta) AS Valor_da_Conta
+    FROM
+            atendime atendime
+        INNER JOIN itreg_amb ON atendime.cd_atendimento = itreg_amb.cd_atendimento
+    WHERE
+            atendime.dt_atendimento BETWEEN ( '01/01/2023' ) AND ( '31/01/2023')
+        AND atendime.cd_convenio LIKE '3'
+        AND atendime.CD_ATENDIMENTO LIKE '3581347'
+        AND cd_lancamento like '9') AS VLR_HO, 
+        SUM(itreg_amb.vl_total_conta) AS Total
+    --remessa_fatura.ds_lote_tiss AS Lote,
+    --tiss_mensagem.nr_protocolo_retorno AS Protocolo,
+    --to_char(fatura.dt_competencia, 'mm-yyyy') AS Competência
 FROM
          atendime atendime
     INNER JOIN paciente ON atendime.cd_paciente = paciente.cd_paciente
     INNER JOIN ori_ate ON atendime.cd_ori_ate = ori_ate.cd_ori_ate
     INNER JOIN convenio ON atendime.cd_convenio = convenio.cd_convenio
-    INNER JOIN reg_fat ON atendime.cd_atendimento = reg_fat.cd_atendimento
     INNER JOIN prestador ON atendime.cd_prestador = prestador.cd_prestador
-    INNER JOIN remessa_fatura ON reg_fat.cd_remessa = remessa_fatura.cd_remessa
-    INNER JOIN tiss_mensagem ON remessa_fatura.ds_lote_tiss = tiss_mensagem.nr_lote
-    INNER JOIN fatura ON remessa_fatura.cd_fatura = fatura.cd_fatura
+    INNER JOIN itreg_amb ON atendime.cd_atendimento = itreg_amb.cd_atendimento
+    --INNER JOIN remessa_fatura ON reg_fat.cd_remessa = remessa_fatura.cd_remessa
+    --INNER JOIN tiss_mensagem ON remessa_fatura.ds_lote_tiss = tiss_mensagem.nr_lote
+    --INNER JOIN fatura ON remessa_fatura.cd_fatura = fatura.cd_fatura
 WHERE
-        atendime.dt_atendimento BETWEEN ( '01/04/2023' ) AND ( '30/04/2023')
-     AND atendime.cd_convenio LIKE '3';
+        atendime.dt_atendimento BETWEEN ( '01/01/2023' ) AND ( '31/01/2023')
+     AND atendime.cd_convenio LIKE '3'
+     AND atendime.CD_ATENDIMENTO LIKE '3581347'
+GROUP BY
+        ori_ate.ds_ori_ate,
+    atendime.cd_atendimento,
+    paciente.nm_paciente,
+    atendime.cd_paciente,
+    paciente.nr_cpf,
+    atendime.dt_atendimento,
+    atendime.dt_alta,
+    convenio.nm_convenio,
+    prestador.nm_prestador,
+    atendime.cd_pro_int;
