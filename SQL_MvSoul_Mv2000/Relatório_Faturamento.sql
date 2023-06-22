0887250026ad8a2b74deff60ee1655d7463918db
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2023-06-22 15:53:33 -0300

    Update Relatório_Faturamento.sql

--- SQL_MvSoul_Mv2000/Relatório_Faturamento.sql
+++ SQL_MvSoul_Mv2000/Relatório_Faturamento.sql
@@ -37,7 +37,9 @@ SELECT
         AND atendime.cd_convenio LIKE '3'
         AND atendime.CD_ATENDIMENTO LIKE '3581347'
         AND cd_lancamento like '9') AS VLR_HO, 
-    SUM(itreg_amb.vl_total_conta) AS Total
+    SUM(itreg_amb.vl_total_conta) AS Total,
+    itreg_amb.cd_reg_amb Conta,
+    reg_amb.cd_remessa Remessa
     --remessa_fatura.ds_lote_tiss AS Lote,
     --tiss_mensagem.nr_protocolo_retorno AS Protocolo,
     --to_char(fatura.dt_competencia, 'mm-yyyy') AS Competência
@@ -48,13 +50,16 @@ FROM
     INNER JOIN convenio ON atendime.cd_convenio = convenio.cd_convenio
     INNER JOIN prestador ON atendime.cd_prestador = prestador.cd_prestador
     INNER JOIN itreg_amb ON atendime.cd_atendimento = itreg_amb.cd_atendimento
+    INNER JOIN reg_amb ON itreg_amb.cd_reg_amb = reg_amb.cd_reg_amb
+    --INNER JOIN remessa ON reg_amb.cd_remessa = remessa_fatura.cd_remessa 
+    --INNER JOIN remessa_fatura ON reg_amb.cd_remessa = remessa_fatura.cd_remessa
     --INNER JOIN remessa_fatura ON reg_fat.cd_remessa = remessa_fatura.cd_remessa
     --INNER JOIN tiss_mensagem ON remessa_fatura.ds_lote_tiss = tiss_mensagem.nr_lote
     --INNER JOIN fatura ON remessa_fatura.cd_fatura = fatura.cd_fatura
 WHERE
         atendime.dt_atendimento BETWEEN ( '01/01/2023' ) AND ( '31/01/2023')
      AND atendime.cd_convenio LIKE '3'
-     AND atendime.CD_ATENDIMENTO LIKE '3581347'
+     AND atendime.cd_atendimento LIKE '3581347'
 GROUP BY
         atendime.cd_ori_ate,
     ori_ate.ds_ori_ate,
@@ -66,4 +71,7 @@ GROUP BY
     atendime.dt_alta,
     convenio.nm_convenio,
     prestador.nm_prestador,
-    atendime.cd_pro_int;
+    atendime.cd_pro_int,
+    reg_amb.cd_remessa,
+    itreg_amb.cd_reg_amb,
+    reg_amb.cd_remessa;
