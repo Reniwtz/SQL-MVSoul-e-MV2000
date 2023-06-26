dc836227cdf0105d6f43a6e3c9221c792e3f311e
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2023-06-26 10:46:41 -0300

    Update Relatório_Faturamento.sql

--- SQL_MvSoul_Mv2000/Relatório_Faturamento.sql
+++ SQL_MvSoul_Mv2000/Relatório_Faturamento.sql
@@ -82,3 +82,37 @@ GROUP BY
     itreg_amb.cd_reg_amb,
     reg_amb.cd_remessa,
     fatura.dt_competencia;
+
+--------------------------------------------------------------------------------------
+
+SELECT
+    atendime.cd_ori_ate AS Numero_Origem,
+    ori_ate.ds_ori_ate AS Origem,
+    atendime.cd_atendimento AS Atendimento,
+    paciente.nm_paciente AS Paciente,
+    atendime.cd_paciente AS CAD_do_Paciente,
+    paciente.nr_cpf AS CPF,
+    atendime.dt_atendimento AS Data_do_Atendimento,
+    atendime.dt_alta AS Data_da_Alta,
+    convenio.nm_convenio AS Convênio,
+    prestador.nm_prestador AS Prestador,
+    atendime.cd_pro_int AS Procedimento,   
+    itreg_amb.vl_total_conta AS Total,
+    itreg_amb.cd_reg_amb AS Conta,
+    reg_amb.cd_remessa AS Remessa,
+    to_char(fatura.dt_competencia, 'mm-yyyy') AS Competência
+FROM
+         atendime atendime
+    INNER JOIN paciente ON atendime.cd_paciente = paciente.cd_paciente
+    INNER JOIN ori_ate ON atendime.cd_ori_ate = ori_ate.cd_ori_ate
+    INNER JOIN convenio ON atendime.cd_convenio = convenio.cd_convenio
+    INNER JOIN prestador ON atendime.cd_prestador = prestador.cd_prestador
+    INNER JOIN itreg_amb ON atendime.cd_atendimento = itreg_amb.cd_atendimento
+    INNER JOIN reg_amb ON itreg_amb.cd_reg_amb = reg_amb.cd_reg_amb
+    INNER JOIN remessa_fatura ON reg_amb.cd_remessa = remessa_fatura.cd_remessa
+    INNER JOIN fatura ON remessa_fatura.cd_fatura = fatura.cd_fatura
+WHERE
+            --atendime.dt_atendimento BETWEEN ( '01/01/2023' ) AND ( '31/01/2023')
+        to_char(fatura.dt_competencia, 'mm/yyyy') = '04/2023'
+     AND atendime.cd_convenio LIKE '3'
+     --AND atendime.cd_atendimento LIKE '3581777'
