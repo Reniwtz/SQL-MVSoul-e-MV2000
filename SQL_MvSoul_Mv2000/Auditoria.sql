2631ccb34768ec36ae0bbd9d8c1e448515c4021a
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2023-08-04 18:03:39 -0300

    Update Auditoria.sql

--- SQL_MvSoul_Mv2000/Auditoria.sql
+++ SQL_MvSoul_Mv2000/Auditoria.sql
@@ -21,11 +21,12 @@ ORDER BY
 ----------------------------------------------------------------------------------------------------
 
 SELECT
-    to_char(audit_dt_registro, 'YYYY-MM-DD HH24:MI:SS'), atendime.*
+    TO_CHAR(audit_dt_registro, 'YYYY-MM-DD HH24:MI:SS') AS data_formatada,
+    atendime.*
 FROM
     audit_dbamv.atendime
 WHERE
-    to char(audit_dt_registro BETWEEN '29/07/2023' AND '04/08/2023'
-    AND cd_atendimento LIKE 3711226
+    audit_dt_registro BETWEEN TO_DATE('29/07/2023', 'DD/MM/YYYY') AND TO_DATE('04/08/2023', 'DD/MM/YYYY')
+    AND cd_atendimento LIKE '3711226%'
 ORDER BY
     audit_dt_registro DESC;
