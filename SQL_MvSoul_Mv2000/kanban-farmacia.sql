ca8c961ff4ab641a14e22252441b701904f7c716
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2023-08-10 18:01:40 -0300

    Create kanban-farmacia.sql

---
+++ SQL_MvSoul_Mv2000/kanban-farmacia.sql
@@ -0,0 +1,37 @@
+SELECT
+    s.cd_solsai_pro                                             solicitacao,
+    decode(s.tp_solsai_pro, 'C', 'Devol. Paciente', 'E', 'Ped. Estoque',
+           'P', 'Ped. Paciente', 'D', 'Devol. Setor', 'S',
+           'Ped. Setor')                                        tipo,
+    decode(s.tp_situacao, 'C', 'Atend. Parcial', 'P', 'Pedido') situacao,
+    se.nm_setor                                                 setor,
+    p.nm_paciente,
+    e.ds_estoque                                                estoque,
+    s.hr_solsai_pro                                             horario,
+    s.cd_pre_med                                                pescricao,
+    decode(s.sn_urgente, 'N', 'Não', 'S', 'Sim')                urgente,
+    i.cd_tip_fre                                                tipo_de_Frequencia,
+    t.ds_tip_fre                                                Descricao
+FROM
+    solsai_pro s,
+    setor      se,
+    pre_med    pm,
+    estoque    e,
+    paciente   p,
+    atendime   a,
+    itpre_med  i,
+    tip_fre    t
+WHERE
+    s.tp_situacao IN ( 'P', 'C' )
+    AND s.cd_setor = se.cd_setor
+    AND s.cd_estoque = e.cd_estoque
+    AND s.cd_pre_med = pm.cd_pre_med (+)
+    AND s.cd_atendimento = a.cd_atendimento (+)
+    AND a.cd_paciente = p.cd_paciente (+)
+    AND pm.cd_pre_med = i.cd_pre_med
+    AND i.cd_tip_fre = t.cd_tip_fre
+    AND s.dt_solsai_pro > ( sysdate - 2 )
+        and s.cd_estoque    in (6)
+ORDER BY
+    s.sn_urgente DESC,
+    s.hr_solsai_pro ASC
