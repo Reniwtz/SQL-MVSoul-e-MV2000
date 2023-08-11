80d6933bbf1ef3b73f1f2d0a81bd78285685ada1
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2023-08-11 12:51:22 -0300

    Update kanban-farmacia.sql

--- SQL_MvSoul_Mv2000/kanban-farmacia.sql
+++ SQL_MvSoul_Mv2000/kanban-farmacia.sql
@@ -1,17 +1,16 @@
 SELECT
-    s.cd_solsai_pro                                             solicitacao,
+    s.cd_solsai_pro solicitacao,
     decode(s.tp_solsai_pro, 'C', 'Devol. Paciente', 'E', 'Ped. Estoque',
-           'P', 'Ped. Paciente', 'D', 'Devol. Setor', 'S',
-           'Ped. Setor')                                        tipo,
-    decode(s.tp_situacao, 'C', 'Atend. Parcial', 'P', 'Pedido') situacao,
-    se.nm_setor                                                 setor,
-    p.nm_paciente,
-    e.ds_estoque                                                estoque,
-    s.hr_solsai_pro                                             horario,
-    s.cd_pre_med                                                pescricao,
-    decode(s.sn_urgente, 'N', 'Não', 'S', 'Sim')                urgente,
-    i.cd_tip_fre                                                tipo_de_Frequencia,
-    t.ds_tip_fre                                                Descricao
+           'P', 'Ped. Paciente', 'D', 'Devol. Setor', 'S','Ped. Setor')tipo,
+    decode(s.tp_situacao, 'C', 'Atend. Parcial', 'P', 'Pedido')situacao,
+    se.nm_setor setor,
+    p.nm_paciente nome_do_paciente,
+    e.ds_estoque estoque,
+    s.hr_solsai_pro horario,
+    s.cd_pre_med pescricao,
+    decode(s.sn_urgente, 'N', 'Não', 'S', 'Sim') urgente,
+    i.cd_tip_fre tipo_de_Frequencia,
+    t.ds_tip_fre descricao
 FROM
     solsai_pro s,
     setor      se,
@@ -31,7 +30,10 @@ WHERE
     AND pm.cd_pre_med = i.cd_pre_med
     AND i.cd_tip_fre = t.cd_tip_fre
     AND s.dt_solsai_pro > ( sysdate - 2 )
-        and s.cd_estoque    in (6)
+    AND s.cd_estoque in (6)
+    AND (t.ds_tip_fre = 'ACM' 
+        OR t.ds_tip_fre = 'SN'
+        OR t.ds_tip_fre = 'AGORA')
 ORDER BY
     s.sn_urgente DESC,
     s.hr_solsai_pro ASC
