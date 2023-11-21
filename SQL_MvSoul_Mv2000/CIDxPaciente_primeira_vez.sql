177fe785fb5121dcaf73df23541210ecc8dc4e1a
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2023-11-21 18:24:30 -0300

    Update CIDxPaciente_primeira_vez.sql

--- SQL_MvSoul_Mv2000/CIDxPaciente_primeira_vez.sql
+++ SQL_MvSoul_Mv2000/CIDxPaciente_primeira_vez.sql
@@ -11,3 +11,39 @@ WHERE
     AND paciente.dt_cadastro BETWEEN TO_DATE('01/01/2023', 'DD/MM/YYYY') AND TO_DATE('31/12/2023', 'DD/MM/YYYY')
 GROUP BY 
     atendime.cd_paciente, atendime.cd_cid
+
+
+SELECT 
+    atendime.cd_cid,
+    cid.ds_cid,
+    count(*)   
+FROM
+    atendime atendime
+    INNER JOIN cid cid ON atendime.cd_cid = cid.cd_cid
+    INNER JOIN paciente on paciente.cd_paciente = atendime.cd_paciente
+WHERE
+     atendime.dt_atendimento BETWEEN TO_DATE('01/01/2022', 'DD/MM/YYYY') AND TO_DATE('31/12/2022', 'DD/MM/YYYY')
+     AND atendime.cd_cid like 'C%'
+    --AND paciente.dt_cadastro BETWEEN TO_DATE('01/01/2023', 'DD/MM/YYYY') AND TO_DATE('31/12/2023', 'DD/MM/YYYY')
+GROUP BY 
+    atendime.cd_cid, cid.ds_cid
+Order by
+    count(*) desc
+    
+    
+SELECT 
+    atendime.cd_cid,
+    cid.ds_cid,
+    count(distinct(atendime.cd_paciente))   
+FROM
+    atendime atendime
+    INNER JOIN cid cid ON atendime.cd_cid = cid.cd_cid
+    INNER JOIN paciente on paciente.cd_paciente = atendime.cd_paciente
+WHERE
+     atendime.dt_atendimento BETWEEN TO_DATE('01/01/2022', 'DD/MM/YYYY') AND TO_DATE('31/12/2022', 'DD/MM/YYYY')
+     AND atendime.cd_cid like 'C%'
+    --AND paciente.dt_cadastro BETWEEN TO_DATE('01/01/2023', 'DD/MM/YYYY') AND TO_DATE('31/12/2023', 'DD/MM/YYYY')
+GROUP BY 
+    atendime.cd_cid, cid.ds_cid
+Order by
+    count(*) desc
