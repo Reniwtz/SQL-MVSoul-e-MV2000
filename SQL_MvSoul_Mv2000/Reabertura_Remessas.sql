b6a8b7dd5cc2c66894739fcbd37ce1e526d36188
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2023-12-19 12:14:36 -0300

    Update Reabertura_Remessas.sql

--- SQL_MvSoul_Mv2000/Reabertura_Remessas.sql
+++ SQL_MvSoul_Mv2000/Reabertura_Remessas.sql
@@ -1,3 +1,6 @@
-  UPDATE dbamv.remessa_bpa
-     SET sn_fechada = 'N' , dt_fechamento = NULL 
-   WHERE cd_remessa IN (1799,1800,1803,1804,1805,1806)
+SELECT * FROM remessa_bpa WHERE cd_remessa IN  (1799,1800,1803,1804,1805,1806)
+
+
+UPDATE dbamv.remessa_bpa
+SET sn_fechada = 'N' , dt_fechamento = NULL 
+WHERE cd_remessa IN (1799,1800,1803,1804,1805,1806)
