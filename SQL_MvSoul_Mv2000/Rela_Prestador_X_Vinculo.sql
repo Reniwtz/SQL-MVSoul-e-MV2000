b1927cf43de3c4678e3fc4a415ab4f1a8a69100b
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2024-01-05 13:09:27 -0300

    Update Rela_Prestador_X_Vinculo.sql

--- SQL_MvSoul_Mv2000/Rela_Prestador_X_Vinculo.sql
+++ SQL_MvSoul_Mv2000/Rela_Prestador_X_Vinculo.sql
@@ -1,19 +1,20 @@
 SELECT
-    *
+    prestador.cd_prestador AS cad_prestador,
+    prestador.nm_prestador AS prestador,
+    prestador.nr_cns       AS cns,
+    prestador.nr_cpf_cgc   AS cpf,
+    prestador_cbo.cd_cbo   AS cbo_vinculado,
+    cbo.ds_cbos            AS descrição
 FROM
-    prestador
+         prestador prestador
+    INNER JOIN prestador_cbo ON prestador.cd_prestador = prestador_cbo.cd_prestador
+    INNER JOIN cbo ON prestador_cbo.cd_cbo = cbo.cd_cbos
 WHERE
-    cd_prestador LIKE '2498';
-
-    
-SELECT
-    cd_prestador AS cad_prestador,
-    nm_prestador  AS prestador,
-    nr_cns        AS cns,
-    nr_cpf_cgc    AS cpf,
-    cd_cbos       AS cbo_vinculado
-FROM
-    prestador
-WHERE
-    cd_prestador like '2498'
-    AND tip_situacao like 'A';
+        prestador.cd_prestador LIKE '2498'
+    AND prestador.tp_situacao LIKE 'A'
+GROUP BY
+    prestador.cd_prestador,  
+    prestador.nm_prestador,
+    prestador.nr_cns,
+    prestador.nr_cpf_cgc,
+    prestador_cbo.cd_cbo;
