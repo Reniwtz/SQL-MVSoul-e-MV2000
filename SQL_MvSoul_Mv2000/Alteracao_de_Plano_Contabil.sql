545832df88d53d201b0c482032021979fee99fbd
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2024-02-05 10:16:15 -0300

    Create Alteracao_de_Plano_Contabil.sql

---
+++ SQL_MvSoul_Mv2000/Alteracao_de_Plano_Contabil.sql
@@ -0,0 +1,5 @@
+SELECT * FROM plano_estr where cd_reduzido = 57
+  
+SELECT * FROM update dbamv.plano_estr set tp_conta = 'S' where cd_reduzido = 57
+  
+SELECT * FROM plano_contas where cd_contabil = '4.1.02.02'
