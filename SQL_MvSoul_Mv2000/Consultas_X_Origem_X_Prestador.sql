121379d7a4617b355ad93174d8a82b4b1ef44857
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2024-02-08 08:30:15 -0300

    Create Consultas_X_Origem_X_Prestador.sql

---
+++ SQL_MvSoul_Mv2000/Consultas_X_Origem_X_Prestador.sql
@@ -0,0 +1,17 @@
+SELECT
+    prestador.nm_prestador,
+    ori_ate.ds_ori_ate,
+    COUNT(*)
+FROM
+    atendime atendime
+    inner join paciente on paciente.cd_paciente = atendime.cd_paciente
+    inner join ori_ate on ori_ate.cd_ori_ate = atendime.cd_ori_ate
+    inner join prestador on prestador.cd_prestador = atendime.cd_prestador
+WHERE
+        atendime.cd_prestador LIKE '226'
+    AND atendime.dt_atendimento BETWEEN TO_DATE('01/01/2024', 'DD/MM/YYYY') AND TO_DATE('31/01/2024', 'DD/MM/YYYY')
+    --AND atendime.tp_atendimento like 'A'
+    AND cd_procedimento like '0301010072'
+group by 
+    prestador.nm_prestador,
+    ori_ate.ds_ori_ate;
