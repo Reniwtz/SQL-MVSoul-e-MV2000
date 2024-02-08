7c3736fcea2e4cb46db452f443b28e55fb5d3b9b
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2024-02-08 15:40:00 -0300

    Update Fpo_Por_Mes_e_Atendimento.sql

--- SQL_MvSoul_Mv2000/Fpo_Por_Mes_e_Atendimento.sql
+++ SQL_MvSoul_Mv2000/Fpo_Por_Mes_e_Atendimento.sql
@@ -51,10 +51,9 @@ FROM
     INNER JOIN teto_orcamentario_proced_sus ON teto_orcamentario_proced_sus.cd_procedimento = procedimento_sus.cd_procedimento
     INNER JOIN fat_sia ON fat_sia.cd_fat_sia = teto_orcamentario_proced_sus.cd_fat_sia 
 WHERE
-    atendime.dt_atendimento BETWEEN TO_DATE('01/04/2023', 'DD/MM/YYYY') AND TO_DATE('30/04/2023', 'DD/MM/YYYY')
-    AND fat_sia.dt_periodo_inicial BETWEEN TO_DATE('01/04/2023', 'DD/MM/YYYY') AND TO_DATE('30/04/2023', 'DD/MM/YYYY')
-    AND ( tp_atendimento LIKE 'A'
-          OR tp_atendimento LIKE 'E' )
+    atendime.dt_atendimento BETWEEN TO_DATE('01/01/2023', 'DD/MM/YYYY') AND TO_DATE('31/01/2023', 'DD/MM/YYYY')
+    AND fat_sia.dt_periodo_inicial BETWEEN TO_DATE('01/01/2023', 'DD/MM/YYYY') AND TO_DATE('31/01/2023', 'DD/MM/YYYY')
+    AND tp_atendimento <> 'I'
     AND atendime.sn_atendimento_apac LIKE 'N'
     AND sn_retorno LIKE 'N'
     AND (atendime.cd_procedimento <> '204030030' 
@@ -103,7 +102,3 @@ GROUP BY
     teto_orcamentario_proced_sus.vl_orcamento 
 ORDER BY
     atendime.cd_procedimento;
-
-
-
-
