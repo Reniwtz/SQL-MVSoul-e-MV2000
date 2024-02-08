1b3f8347ab3d01ba6cb1ee5752e9680f9b2b0482
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2024-02-08 15:50:23 -0300

    Update Obitos_de _Não_Internos.sql

--- SQL_MvSoul_Mv2000/Obitos_de _Não_Internos.sql
+++ SQL_MvSoul_Mv2000/Obitos_de _Não_Internos.sql
@@ -5,13 +5,15 @@ SELECT
     paciente.dt_nascimento                                      AS data_nascimento,
     paciente.tp_sexo                                            AS sexo,
     ori_ate.ds_ori_ate                                          AS origem_atendimento,
-    trunc(months_between(sysdate, paciente.dt_nascimento) / 12) AS idade
+    trunc(months_between(sysdate, paciente.dt_nascimento) / 12) AS idade,
+    convenio.nm_convenio                                        AS convênio
 FROM
          atendime atendime
     INNER JOIN paciente ON atendime.cd_paciente = paciente.cd_paciente
     INNER JOIN ori_ate ON atendime.cd_ori_ate = ori_ate.cd_ori_ate
+    INNER JOIN convenio ON atendime.cd_convenio  = convenio.cd_convenio
 WHERE
-    atendime.dt_alta BETWEEN TO_DATE('01/01/24', 'DD/MM/YY') AND TO_DATE('31/12/24', 'DD/MM/YY')
+    atendime.dt_alta BETWEEN TO_DATE('01/01/23', 'DD/MM/YY') AND TO_DATE('31/01/23', 'DD/MM/YY')
     AND atendime.tp_atendimento <> 'I'
     AND atendime.sn_obito = 'S'
 GROUP BY
@@ -21,5 +23,6 @@ GROUP BY
     paciente.dt_nascimento,
     paciente.tp_sexo,
     ori_ate.ds_ori_ate,
-    trunc(months_between(sysdate, paciente.dt_nascimento) / 12)
+    trunc(months_between(sysdate, paciente.dt_nascimento) / 12),
+    convenio.nm_convenio 
     
