b3e9d1bff150ad483ee07c0917123efdc5a7940c
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2024-02-08 13:05:32 -0300

    Update Obitos_de _Não_Internos.sql

--- SQL_MvSoul_Mv2000/Obitos_de _Não_Internos.sql
+++ SQL_MvSoul_Mv2000/Obitos_de _Não_Internos.sql
@@ -1,24 +1,25 @@
 SELECT
-    aten.cd_atendimento                                    AS atendimento,
-    pac.nm_paciente                                        AS nome_paciente,
-    dt_alta                                                AS data_obito,
-    pac.dt_nascimento                                      AS data_nascimento,
-    tp_sexo                                                AS sexo,
-    ori.ds_ori_ate                                         AS origem_atendimento,
-    trunc(months_between(sysdate, pac.dt_nascimento) / 12) AS idade
+    atendime.cd_atendimento                                     AS atendimento,
+    paciente.nm_paciente                                        AS nome_paciente,
+    atendime.dt_alta                                            AS data_obito,
+    paciente.dt_nascimento                                      AS data_nascimento,
+    paciente.tp_sexo                                            AS sexo,
+    ori_ate.ds_ori_ate                                          AS origem_atendimento,
+    trunc(months_between(sysdate, paciente.dt_nascimento) / 12) AS idade
 FROM
-         atendime aten
-    INNER JOIN paciente pac ON aten.cd_paciente = pac.cd_paciente
-    INNER JOIN ori_ate  ori ON aten.cd_ori_ate = ori.cd_ori_ate
+         atendime atendime
+    INNER JOIN paciente ON atendime.cd_paciente = paciente.cd_paciente
+    INNER JOIN ori_ate ON atendime.cd_ori_ate = ori_ate.cd_ori_ate
 WHERE
-    TO_DATE(dt_alta, 'DD/MM/YY') BETWEEN TO_DATE('01/01/24', 'DD/MM/YY') AND TO_DATE('31/12/24', 'DD/MM/YY')
-    AND tp_atendimento <> 'I'
-    AND sn_obito = 'S'
+    atendime.dt_alta BETWEEN TO_DATE('01/01/24', 'DD/MM/YY') AND TO_DATE('31/12/24', 'DD/MM/YY')
+    AND atendime.tp_atendimento <> 'I'
+    AND atendime.sn_obito = 'S'
 GROUP BY
-    aten.cd_atendimento,
-    pac.nm_paciente,
-    dt_alta,
-    pac.dt_nascimento,
-    tp_sexo,
-    ori.ds_ori_ate,
-    trunc(months_between(sysdate, pac.dt_nascimento) / 12)
+    atendime.cd_atendimento,
+    paciente.nm_paciente,
+    atendime.dt_alta,
+    paciente.dt_nascimento,
+    paciente.tp_sexo,
+    ori_ate.ds_ori_ate,
+    trunc(months_between(sysdate, paciente.dt_nascimento) / 12)
+    
