85a89f6c36f7b61edba06a01e9d7b9ea9138138b
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2024-02-16 11:25:57 -0300

    Update Atendimentos_de_Primeira_Vez.sql

--- SQL_MvSoul_Mv2000/Atendimentos_de_Primeira_Vez.sql
+++ SQL_MvSoul_Mv2000/Atendimentos_de_Primeira_Vez.sql
@@ -7,41 +7,41 @@ FROM
     paciente p,
     cidade   c
 WHERE
-    p.dt_cadastro BETWEEN ( '01/01/2021' ) AND ( '31/12/2021' )
+    p.dt_cadastro BETWEEN TO_DATE('01/01/23', 'DD/MM/YY') AND TO_DATE('31/12/23', 'DD/MM/YY')
     AND c.cd_cidade = p.cd_cidade
     AND c.cd_uf = 'PB'
     AND ( nm_cidade = 'JOAO PESSOA'
           OR c.nm_cidade = 'CABEDELO'
-          OR c.nm_cidade = 'SANTA RITA'
           OR c.nm_cidade = 'BAYEUX'
+          OR c.nm_cidade = 'SANTA RITA'
           OR c.nm_cidade = 'GUARABIRA'
-          OR c.nm_cidade = 'PATOS'
+          OR c.nm_cidade = 'CONDE'
           OR c.nm_cidade = 'MAMANGUAPE'
           OR c.nm_cidade = 'SAPE'
-          OR c.nm_cidade = 'ITABAIANA'
-          OR c.nm_cidade = 'JURIPIRANGA'
-          OR c.nm_cidade = 'CONDE'
+          OR c.nm_cidade = 'GUARABIRA'
+          OR c.nm_cidade = 'PEDRAS DE FOGO'
+          OR c.nm_cidade = 'BAIA DA TRAICAO'
+          OR c.nm_cidade = 'PATOS'
+          OR c.nm_cidade = 'ALHANDRA'
+          OR c.nm_cidade = 'CAJAZEIRAS'
           OR c.nm_cidade = 'CAMPINA GRANDE'
           OR c.nm_cidade = 'RIO TINTO'
-          OR c.nm_cidade = 'ALHANDRA'
-          OR c.nm_cidade = 'LUCENA'
-          OR c.nm_cidade = 'LUCENA'
-          OR c.nm_cidade = 'PEDRAS DE FOGO'
-          OR c.nm_cidade = 'PILAR'
           OR c.nm_cidade = 'CAAPORA'
-          OR c.nm_cidade = 'SOUSA'
-          OR c.nm_cidade = 'SALGADO DE SAO FELIX' )
+          OR c.nm_cidade = 'LUCENA'
+          OR c.nm_cidade = 'GURINHEM'
+          OR c.nm_cidade = 'ITAPOROROCA'
+          OR c.nm_cidade = 'SOUSA' )
 GROUP BY
     c.nm_cidade,
     c.cd_uf
 ORDER BY
-    COUNT(*);
+    COUNT(*) desc;
     
     
 -- Total de atendimento de primeira vez
 SELECT COUNT(p.cd_paciente)
   FROM paciente p
- WHERE p.dt_cadastro BETWEEN ( '01/01/2021' ) AND ( '31/12/2021' );
+ WHERE p.dt_cadastro BETWEEN TO_DATE('01/01/23', 'DD/MM/YY') AND TO_DATE('31/12/23', 'DD/MM/YY');
 
 
 -- Total de atendimento de primeira vez por Municipio especifico
@@ -51,27 +51,27 @@ FROM
     paciente p,
     cidade   c
 WHERE
-    p.dt_cadastro BETWEEN ( '01/01/2021' ) AND ( '31/12/2021' )
+    p.dt_cadastro BETWEEN TO_DATE('01/01/23', 'DD/MM/YY') AND TO_DATE('31/12/23', 'DD/MM/YY')
     AND c.cd_cidade = p.cd_cidade
     AND c.cd_uf = 'PB'
-    AND ( nm_cidade = 'JOAO PESSOA'
+        AND ( nm_cidade = 'JOAO PESSOA'
           OR c.nm_cidade = 'CABEDELO'
-          OR c.nm_cidade = 'SANTA RITA'
           OR c.nm_cidade = 'BAYEUX'
+          OR c.nm_cidade = 'SANTA RITA'
           OR c.nm_cidade = 'GUARABIRA'
-          OR c.nm_cidade = 'PATOS'
+          OR c.nm_cidade = 'CONDE'
           OR c.nm_cidade = 'MAMANGUAPE'
           OR c.nm_cidade = 'SAPE'
-          OR c.nm_cidade = 'ITABAIANA'
-          OR c.nm_cidade = 'JURIPIRANGA'
-          OR c.nm_cidade = 'CONDE'
+          OR c.nm_cidade = 'GUARABIRA'
+          OR c.nm_cidade = 'PEDRAS DE FOGO'
+          OR c.nm_cidade = 'BAIA DA TRAICAO'
+          OR c.nm_cidade = 'PATOS'
+          OR c.nm_cidade = 'ALHANDRA'
+          OR c.nm_cidade = 'CAJAZEIRAS'
           OR c.nm_cidade = 'CAMPINA GRANDE'
           OR c.nm_cidade = 'RIO TINTO'
-          OR c.nm_cidade = 'ALHANDRA'
-          OR c.nm_cidade = 'LUCENA'
-          OR c.nm_cidade = 'LUCENA'
-          OR c.nm_cidade = 'PEDRAS DE FOGO'
-          OR c.nm_cidade = 'PILAR'
           OR c.nm_cidade = 'CAAPORA'
-          OR c.nm_cidade = 'SOUSA'
-          OR c.nm_cidade = 'SALGADO DE SAO FELIX' );
+          OR c.nm_cidade = 'LUCENA'
+          OR c.nm_cidade = 'GURINHEM'
+          OR c.nm_cidade = 'ITAPOROROCA'
+          OR c.nm_cidade = 'SOUSA' )
