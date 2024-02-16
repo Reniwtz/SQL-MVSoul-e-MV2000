f6056813c5db3f58b94e1bd6fee73ece01bb11ca
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2024-02-16 13:02:07 -0300

    Update Obitos_de _Não_Internos.sql

--- SQL_MvSoul_Mv2000/Obitos_de _Não_Internos.sql
+++ SQL_MvSoul_Mv2000/Obitos_de _Não_Internos.sql
@@ -14,16 +14,16 @@ FROM
     INNER JOIN ori_ate ON atendime.cd_ori_ate = ori_ate.cd_ori_ate
     INNER JOIN convenio ON atendime.cd_convenio  = convenio.cd_convenio
 WHERE
-        atendime.dt_alta BETWEEN TO_DATE('01/01/23', 'DD/MM/YY') AND TO_DATE('31/01/23', 'DD/MM/YY')
+        atendime.dt_alta BETWEEN TO_DATE('01/01/23', 'DD/MM/YY') AND TO_DATE('31/12/23', 'DD/MM/YY')
     AND atendime.tp_atendimento <> 'I'
     AND atendime.sn_obito = 'S'
 GROUP BY
-    atendime.cd_atendimento,
+     atendime.cd_paciente,
     paciente.nm_paciente,
+    atendime.cd_atendimento,
     atendime.dt_alta,
     paciente.dt_nascimento,
     paciente.tp_sexo,
     ori_ate.ds_ori_ate,
     trunc(months_between(sysdate, paciente.dt_nascimento) / 12),
-    convenio.nm_convenio 
-    
+    convenio.nm_convenio      
