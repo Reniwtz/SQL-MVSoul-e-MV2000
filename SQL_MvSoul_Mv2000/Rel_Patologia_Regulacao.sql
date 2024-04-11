700550fa551017704227d947bda3f786b00f0c03
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2024-04-11 15:08:44 -0300

    Create Rel_Patologia_Regulacao.sql

---
+++ SQL_MvSoul_Mv2000/Rel_Patologia_Regulacao.sql
@@ -0,0 +1,27 @@
+SELECT
+    atendime.cd_procedimento         AS procedimento,
+    procedimento_sus.ds_procedimento AS descricao,
+    COUNT(*)                         AS quantidade,
+    valor                            AS valor_individual,
+    ( valor * COUNT(*) )             AS valor_total
+FROM
+         ped_rx ped_rx
+    INNER JOIN atendime ON ped_rx.cd_atendimento = atendime.cd_atendimento
+    INNER JOIN procedimento_sus ON procedimento_sus.cd_procedimento = atendime.cd_procedimento
+    LEFT JOIN (
+        SELECT
+            cd_procedimento,
+            vl_orcamento / qt_fisico AS valor
+        FROM
+                 teto_orcamentario_proced_sus
+            INNER JOIN fat_sia ON teto_orcamentario_proced_sus.cd_fat_sia = fat_sia.cd_fat_sia
+        WHERE
+            to_char(fat_sia.dt_periodo_inicial, 'MM/YYYY') = '03/2024'
+    ) valor ON valor.cd_procedimento = atendime.cd_procedimento
+WHERE
+    ped_rx.ds_observacao LIKE 'REGU%'
+    AND ped_rx.dt_pedido BETWEEN TO_DATE('01/01/24', 'DD/MM/YY') AND TO_DATE('31/01/24', 'DD/MM/YY')
+GROUP BY
+    atendime.cd_procedimento,
+    procedimento_sus.ds_procedimento,
+    valor;
