735547c686eaa2cd12afc5ddffcf2fd2c80bb67c
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2024-04-15 17:33:23 -0300

    Update Procedimentos_idade_paciente.sql

--- SQL_MvSoul_Mv2000/Procedimentos_idade_paciente.sql
+++ SQL_MvSoul_Mv2000/Procedimentos_idade_paciente.sql
@@ -7,10 +7,10 @@ FROM
     INNER JOIN paciente ON paciente.cd_paciente = atendime.cd_paciente
     INNER JOIN procedimento_sus ON procedimento_sus.cd_procedimento = atendime.cd_procedimento
 WHERE
-    atendime.dt_atendimento BETWEEN TO_DATE('01/01/24', 'DD/MM/YY') AND TO_DATE('31/12/24', 'DD/MM/YY')
+    atendime.dt_atendimento BETWEEN TO_DATE('01/01/22', 'DD/MM/YY') AND TO_DATE('31/12/22', 'DD/MM/YY')
     AND atendime.cd_procedimento LIKE '0304010413'
-    AND dt_nascimento > TO_DATE('31/12/1988', 'DD/MM/YY')
+    AND EXTRACT(YEAR FROM SYSDATE) - EXTRACT(YEAR FROM dt_nascimento) >= 80
 GROUP BY
     atendime.cd_paciente,
     paciente.nm_paciente,
-    paciente.dt_nascimento
+    paciente.dt_nascimento;
