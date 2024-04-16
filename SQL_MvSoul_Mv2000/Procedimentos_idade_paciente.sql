d259139cbd129750d6af9a3d8f5231d26a211c92
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2024-04-16 16:33:33 -0300

    Update Procedimentos_idade_paciente.sql

--- SQL_MvSoul_Mv2000/Procedimentos_idade_paciente.sql
+++ SQL_MvSoul_Mv2000/Procedimentos_idade_paciente.sql
@@ -14,3 +14,34 @@ GROUP BY
     atendime.cd_paciente,
     paciente.nm_paciente,
     paciente.dt_nascimento;
+
+
+-----------------------------------------------------------------------------------------------------
+
+SELECT
+    atendime.cd_paciente,
+    paciente.dt_nascimento,
+    grau_ins.ds_grau_ins,
+    paciente.ds_endereco,
+    cidade.nm_cidade,
+    paciente.tp_estado_civil,
+    raca_cor.nm_raca_cor
+FROM
+         atendime atendime
+    INNER JOIN paciente ON paciente.cd_paciente = atendime.cd_paciente
+    INNER JOIN procedimento_sus ON procedimento_sus.cd_procedimento = atendime.cd_procedimento
+    INNER JOIN grau_ins ON grau_ins.cd_grau_ins = paciente.cd_grau_ins
+    INNER JOIN cidade ON cidade.cd_cidade = paciente.cd_cidade
+    INNER JOIN raca_cor ON raca_cor.tp_cor = paciente.tp_cor
+WHERE
+    atendime.dt_atendimento BETWEEN TO_DATE('01/01/22', 'DD/MM/YY') AND TO_DATE('31/12/22', 'DD/MM/YY')
+    AND atendime.cd_procedimento LIKE '0304010456'
+    AND EXTRACT(YEAR FROM sysdate) - EXTRACT(YEAR FROM dt_nascimento) >= 60
+GROUP BY
+    atendime.cd_paciente,
+    paciente.dt_nascimento,
+    grau_ins.ds_grau_ins,
+    paciente.ds_endereco,
+    cidade.nm_cidade,
+    paciente.tp_estado_civil,
+    raca_cor.nm_raca_cor
