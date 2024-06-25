1ccffd1d1928e5f62bcb7bb3d5773b447fbfdb6e
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2024-06-25 11:06:52 -0300

    Update Comprovante_PEP_Oncologico.sql

--- SQL_MvSoul_Mv2000/Comprovante_PEP_Oncologico.sql
+++ SQL_MvSoul_Mv2000/Comprovante_PEP_Oncologico.sql
@@ -1,20 +1,21 @@
 SELECT
-    paciente.cd_paciente                   AS código_do_paciente,
-    paciente.nm_paciente                   AS nome_do_paciente,
-    paciente.dt_nascimento                 AS data_de_nacimento,
-    tipo_sexo.nm_sexo                      AS sexo,
-    paciente.nr_ddd_fone                   AS dd,
-    paciente.nr_fone                       AS telefone,
-    solic_agendamento.cd_solic_agendamento AS número_da_solicitação,
-    solic_agendamento.dt_solic_agendamento AS data,
-    solic_agendamento.cd_atendimento       AS atendimento,
-    prestador.nm_prestador                 AS médico,
-    ori_ate.ds_ori_ate                     AS setor,
-    convenio.nm_convenio                   AS convênio,
-    con_pla.ds_con_pla                     AS plano,
-    tip_mar.ds_tip_mar                     AS tipo_de_atendimento,
-    solic_agendamento.cd_item_agendamento  AS item_de_agendamento,
-    item_agendamento.ds_item_agendamento   AS descrição
+    paciente.cd_paciente                             AS código_do_paciente,
+    paciente.nm_paciente                             AS nome_do_paciente,
+    paciente.dt_nascimento                           AS data_de_nacimento,
+    tipo_sexo.nm_sexo                                AS sexo,
+    paciente.nr_ddd_fone                             AS dd,
+    paciente.nr_fone                                 AS telefone,
+    solic_agendamento.cd_solic_agendamento           AS número_da_solicitação,
+    agendamento_oncologico.dh_inicio_agendamento_onc AS data,
+    solic_agendamento.cd_atendimento                 AS atendimento,
+    prestador.nm_prestador                           AS médico,
+    ori_ate.ds_ori_ate                               AS setor,
+    convenio.nm_convenio                             AS convênio,
+    con_pla.ds_con_pla                               AS plano,
+    tip_mar.ds_tip_mar                               AS tipo_de_atendimento,
+    solic_agendamento.cd_item_agendamento            AS item_de_agendamento,
+    item_agendamento.ds_item_agendamento             AS descrição,
+    recurso_oncologico.ds_recurso_oncologico         AS recurso
 FROM
          solic_agendamento solic_agendamento
     INNER JOIN item_agendamento ON item_agendamento.cd_item_agendamento = solic_agendamento.cd_item_agendamento
@@ -26,5 +27,7 @@ FROM
     INNER JOIN con_pla ON con_pla.cd_convenio = atendime.cd_convenio
     INNER JOIN ori_ate ON ori_ate.cd_ori_ate = atendime.cd_ori_ate
     INNER JOIN tip_mar ON tip_mar.cd_tip_mar = atendime.cd_tip_mar
+    INNER JOIN agendamento_oncologico ON agendamento_oncologico.cd_solic_agendamento = solic_agendamento.cd_solic_agendamento
+    INNER JOIN recurso_oncologico ON recurso_oncologico.cd_recurso_oncologico = agendamento_oncologico.cd_recurso_oncologico
 WHERE
     solic_agendamento.cd_atendimento LIKE '3922433'
