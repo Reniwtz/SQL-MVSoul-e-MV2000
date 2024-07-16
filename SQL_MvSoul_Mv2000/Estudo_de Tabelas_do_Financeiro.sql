089b4f11dde24e9e92930c08940b38bab6f87c6c
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2024-07-16 13:55:13 -0300

    Update Estudo_de Tabelas_do_Financeiro.sql

--- SQL_MvSoul_Mv2000/Estudo_de Tabelas_do_Financeiro.sql
+++ SQL_MvSoul_Mv2000/Estudo_de Tabelas_do_Financeiro.sql
@@ -13,10 +13,47 @@ SELECT
     cd_usuario,
     dt_lancamento
 FROM
-    con_rec
+    con_rec con_rec
 WHERE
     dt_emissao BETWEEN TO_DATE('01/07/2024', 'DD/MM/YYYY') AND TO_DATE('15/07/2024', 'DD/MM/YYYY')
 ORDER BY
-    cd_con_rec DESC
+    cd_con_rec DESC;
+    
+--------------------------------------------------------------------------------
 
--------------------------------------------------------------------------------------------------
+SELECT
+    cd_reccon_rec,
+    cd_itcon_rec,
+    nr_documento,
+    ds_reccon_rec,
+    cd_fin_car,
+    cd_caixa,
+    tp_recebimento,
+    dt_recebimento,
+    vl_recebido,
+    cd_exp_contabilidade,
+    cd_caucao,
+    vl_moeda,
+    cd_doc_caixa,
+    cd_mov_caixa,
+    cd_processo
+FROM
+    reccon_rec reccon_rec
+WHERE
+    dt_recebimento BETWEEN TO_DATE('01/07/2024', 'DD/MM/YYYY') AND TO_DATE('15/07/2024', 'DD/MM/YYYY');
+ 
+--------------------------------------------------------------------------------
+
+SELECT
+    cd_itcon_rec,
+    cd_con_rec,
+    nr_parcela,
+    dt_prevista_recebimento,
+    dt_vencimento,
+    vl_duplicata,
+    vl_soma_recebido
+FROM
+    itcon_rec
+WHERE
+    dt_prevista_recebimento BETWEEN TO_DATE('01/07/2024', 'DD/MM/YYYY') AND TO_DATE('15/07/2024', 'DD/MM/YYYY');
+ 
