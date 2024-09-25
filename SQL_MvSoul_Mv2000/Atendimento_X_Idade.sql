c82f6bf804f4cc54405de2c71a5275d1ef0f90be
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2024-09-25 11:43:33 -0300

    Update Atendimento_X_Idade.sql

--- SQL_MvSoul_Mv2000/Atendimento_X_Idade.sql
+++ SQL_MvSoul_Mv2000/Atendimento_X_Idade.sql
@@ -14,23 +14,24 @@ GROUP BY
 SELECT
     atendime.cd_paciente,
     atendime.dt_atendimento,
-    FLOOR((EXTRACT(YEAR FROM atendime.dt_atendimento) - EXTRACT(YEAR FROM paciente.dt_nascimento)) - 
-          CASE 
-              WHEN EXTRACT(MONTH FROM atendime.dt_atendimento) < EXTRACT(MONTH FROM paciente.dt_nascimento) 
-              OR (EXTRACT(MONTH FROM atendime.dt_atendimento) = EXTRACT(MONTH FROM paciente.dt_nascimento) 
-              AND EXTRACT(DAY FROM atendime.dt_atendimento) < EXTRACT(DAY FROM paciente.dt_nascimento))
-              THEN 1 
-              ELSE 0 
-          END) AS idade
+    floor((EXTRACT(YEAR FROM atendime.dt_atendimento) - EXTRACT(YEAR FROM paciente.dt_nascimento)) -
+          CASE
+              WHEN EXTRACT(MONTH FROM atendime.dt_atendimento) < EXTRACT(MONTH FROM paciente.dt_nascimento)
+                   OR(EXTRACT(MONTH FROM atendime.dt_atendimento) = EXTRACT(MONTH FROM paciente.dt_nascimento)
+                      AND EXTRACT(DAY FROM atendime.dt_atendimento) < EXTRACT(DAY FROM paciente.dt_nascimento)) THEN
+                  1
+              ELSE
+                  0
+          END
+    ) AS idade
 FROM
-    atendime atendime
-INNER JOIN 
-    paciente ON paciente.cd_paciente = atendime.cd_paciente
+         atendime atendime
+    INNER JOIN paciente ON paciente.cd_paciente = atendime.cd_paciente
 WHERE
     atendime.dt_atendimento BETWEEN TO_DATE('01/01/24', 'DD/MM/YY') AND TO_DATE('31/12/24', 'DD/MM/YY')
 GROUP BY
     atendime.cd_paciente,
     atendime.dt_atendimento,
     paciente.dt_nascimento
-order by
-    idade desc;
+ORDER BY
+    idade DESC;
