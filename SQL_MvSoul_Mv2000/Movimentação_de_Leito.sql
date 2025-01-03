bc0dad8bcbf55da5ab79be7e9b59ee59fc47f450
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2025-01-03 16:30:20 -0300

    Update Movimentação_de_Leito.sql

--- SQL_MvSoul_Mv2000/Movimentação_de_Leito.sql
+++ SQL_MvSoul_Mv2000/Movimentação_de_Leito.sql
@@ -16,3 +16,38 @@ GROUP BY
 ORDER BY
     mov_int.hr_mov_int DESC,
     leito.ds_leito;
+
+-----------------------------------------------------------------------------
+SELECT
+    mov_int.cd_atendimento                                      AS código_do_atendimento,
+    paciente.cd_paciente                                        AS código_do_paciente,
+    paciente.nm_paciente                                        AS nome_do_paciente,
+    trunc(months_between(sysdate, paciente.dt_nascimento) / 12) AS idade,
+    paciente.tp_sexo                                            AS genero,
+    leito.cd_leito                                              AS codigo_do_leito,
+    leito.ds_leito                                              AS nome_do_leito,
+    mov_int.hr_mov_int                                          AS horario_da_movimentacao,
+    atendime.dt_alta                                            AS data_da_alta
+FROM
+         leito leito
+    INNER JOIN mov_int ON leito.cd_leito = mov_int.cd_leito
+    INNER JOIN atendime ON mov_int.cd_atendimento = atendime.cd_atendimento
+    INNER JOIN paciente ON atendime.cd_paciente = paciente.cd_paciente
+WHERE
+    leito.ds_leito LIKE '%UTI 00%'
+    AND leito.tp_situacao LIKE '%A%'
+    --mov_int.cd_convenio LIKE '1'
+    AND mov_int.hr_mov_int BETWEEN TO_DATE('01/12/2024', 'DD/MM/YYYY') AND TO_DATE('31/12/2024', 'DD/MM/YYYY')
+GROUP BY
+    mov_int.cd_atendimento,
+    paciente.cd_paciente,
+    trunc(months_between(sysdate, paciente.dt_nascimento) / 12),
+    paciente.tp_sexo,
+    paciente.nm_paciente,
+    leito.cd_leito,
+    leito.ds_leito,
+    mov_int.hr_mov_int,
+    atendime.dt_alta
+ORDER BY
+    mov_int.cd_atendimento,
+    mov_int.hr_mov_int DESC
