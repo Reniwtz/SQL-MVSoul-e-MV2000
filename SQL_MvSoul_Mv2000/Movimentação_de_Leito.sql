07e1d57e51a5b01de59ea1b29948ee10de07f423
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2025-01-10 09:41:25 -0300

    Update Movimentação_de_Leito.sql

--- SQL_MvSoul_Mv2000/Movimentação_de_Leito.sql
+++ SQL_MvSoul_Mv2000/Movimentação_de_Leito.sql
@@ -32,9 +32,10 @@ SELECT
     pro_fat.ds_pro_fat                                          AS descrição,
     leito.cd_leito                                              AS codigo_do_leito,
     leito.ds_leito                                              AS nome_do_leito,
+    atendime.hr_atendimento                                     AS data_do_atendimento,
     mov_int.hr_mov_int                                          AS horario_da_movimentacao,
-    atendime.dt_alta                                            AS data_da_alta,
-    log_alta.tp_alta                                            AS tipo_de_alta
+    atendime.dt_alta_medica                                     AS data_da_alta,
+    mot_alt.ds_mot_alt                                          AS tipo_de_alta
 FROM
          leito leito
     INNER JOIN mov_int ON leito.cd_leito = mov_int.cd_leito
@@ -43,11 +44,10 @@ FROM
     LEFT JOIN procedimento_sus ON atendime.cd_procedimento = procedimento_sus.cd_procedimento
     LEFT JOIN pro_fat ON atendime.cd_pro_int = pro_fat.cd_pro_fat
     LEFT JOIN cid ON atendime.cd_cid = cid.cd_cid
-    LEFT JOIN log_alta ON atendime.cd_atendimento = log_alta.cd_atendimento
+    LEFT JOIN mot_alt ON atendime.cd_mot_alt = mot_alt.cd_mot_alt
 WHERE
     leito.ds_leito LIKE '%UTI 00%'
     AND leito.tp_situacao LIKE '%A%'
-    --mov_int.cd_convenio LIKE '1'
     AND mov_int.hr_mov_int BETWEEN TO_DATE('01/12/2024', 'DD/MM/YYYY') AND TO_DATE('31/12/2024', 'DD/MM/YYYY')
 GROUP BY
     mov_int.cd_atendimento,
@@ -63,9 +63,67 @@ GROUP BY
     paciente.nm_paciente,
     leito.cd_leito,
     leito.ds_leito,
+    atendime.hr_atendimento,
     mov_int.hr_mov_int,
-    atendime.dt_alta,
-    log_alta.tp_alta
-ORDER BY
+    atendime.dt_alta_medica,
+    mot_alt.ds_mot_alt
+UNION
+SELECT
+    mov_int.cd_atendimento                                      AS código_do_atendimento,
+    paciente.cd_paciente                                        AS código_do_paciente,
+    paciente.nm_paciente                                        AS nome_do_paciente,
+    trunc(months_between(sysdate, paciente.dt_nascimento) / 12) AS idade,
+    paciente.tp_sexo                                            AS genero,
+    atendime.cd_cid                                             AS cid_da_admissao,
+    cid.ds_cid                                                  AS descrição,
+    atendime.cd_procedimento                                    AS cdg_do_proced_sus,
+    procedimento_sus.ds_procedimento                            AS descrição,
+    atendime.cd_pro_int                                         AS cdg_do_proced_conv_partic,
+    pro_fat.ds_pro_fat                                          AS descrição,
+    leito.cd_leito                                              AS codigo_do_leito,
+    leito.ds_leito                                              AS nome_do_leito,
+    atendime.hr_atendimento                                     AS data_do_atendimento,
+    mov_int.hr_mov_int                                          AS horario_da_movimentacao,
+    atendime.dt_alta_medica                                     AS data_da_alta,
+    mot_alt.ds_mot_alt                                          AS tipo_de_alta
+FROM
+         leito leito
+    INNER JOIN mov_int ON leito.cd_leito = mov_int.cd_leito
+    INNER JOIN atendime ON mov_int.cd_atendimento = atendime.cd_atendimento
+    INNER JOIN paciente ON atendime.cd_paciente = paciente.cd_paciente
+    LEFT JOIN procedimento_sus ON atendime.cd_procedimento = procedimento_sus.cd_procedimento
+    LEFT JOIN pro_fat ON atendime.cd_pro_int = pro_fat.cd_pro_fat
+    LEFT JOIN cid ON atendime.cd_cid = cid.cd_cid
+    LEFT JOIN mot_alt ON atendime.cd_mot_alt = mot_alt.cd_mot_alt
+WHERE
+    atendime.dt_atendimento BETWEEN TO_DATE('01/12/2024', 'DD/MM/YYYY') AND TO_DATE('31/12/2024', 'DD/MM/YYYY')
+    AND ( atendime.cd_leito LIKE '351'
+          OR atendime.cd_leito LIKE '352'
+          OR atendime.cd_leito LIKE '216'
+          OR atendime.cd_leito LIKE '194'
+          OR atendime.cd_leito LIKE '195'
+          OR atendime.cd_leito LIKE '196'
+          OR atendime.cd_leito LIKE '197'
+          OR atendime.cd_leito LIKE '199'
+          OR atendime.cd_leito LIKE '200' )
+GROUP BY
     mov_int.cd_atendimento,
-    mov_int.hr_mov_int DESC
+    paciente.cd_paciente,
+    trunc(months_between(sysdate, paciente.dt_nascimento) / 12),
+    paciente.tp_sexo,
+    atendime.cd_cid,
+    cid.ds_cid,
+    atendime.cd_procedimento,
+    procedimento_sus.ds_procedimento,
+    atendime.cd_pro_int,
+    pro_fat.ds_pro_fat,
+    paciente.nm_paciente,
+    leito.cd_leito,
+    leito.ds_leito,
+    atendime.hr_atendimento,
+    mov_int.hr_mov_int,
+    atendime.dt_alta_medica,
+    mot_alt.ds_mot_alt
+ORDER BY
+    código_do_atendimento,
+    horario_da_movimentacao DESC;
