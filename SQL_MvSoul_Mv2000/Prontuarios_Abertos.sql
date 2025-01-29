ae5fe452d23801720a9dafdbf81018734bb182bb
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2025-01-29 09:32:13 -0300

    Update Prontuarios_Abertos.sql

--- SQL_MvSoul_Mv2000/Prontuarios_Abertos.sql
+++ SQL_MvSoul_Mv2000/Prontuarios_Abertos.sql
@@ -1,12 +1,21 @@
-select DISTINCT(NR_MATRICULA_volume) from same
- where dt_cadastro between '01/07/2022' and '31/07/2022' and NR_VOLUME = 1;
-
-
-select * from same
- where dt_cadastro between '01/08/2022' and '31/08/2022';
-
-select dt_cadastro, count(nr_matricula_same)
-  from same 
- where dt_cadastro between '02/12/2016' and '02/12/2016'
-group by dt_cadastro
-order by dt_cadastro;
+SELECT
+    same.nr_matricula_same,
+    same.cd_paciente,
+    paciente.nm_paciente,
+    same.dt_cadastro dt_same
+FROM
+         same same
+    INNER JOIN paciente ON paciente.cd_paciente = same.cd_paciente
+    INNER JOIN it_same ON same.nr_matricula_same = it_same.nr_matricula_same
+    INNER JOIN atendime ON it_same.cd_atendimento = atendime.cd_atendimento
+WHERE
+        same.nr_matricula_same >= 107001
+    AND same.dt_cadastro BETWEEN TO_DATE('29/01/2025', 'DD/MM/YYYY') AND TO_DATE('29/01/2025', 'DD/MM/YYYY')
+    --AND trunc(months_between(sysdate, paciente.dt_nascimento) / 12) >= 18 
+GROUP BY
+    same.nr_matricula_same,
+    same.cd_paciente,
+    paciente.nm_paciente,
+    same.dt_cadastro
+order by
+    paciente.nm_paciente;
