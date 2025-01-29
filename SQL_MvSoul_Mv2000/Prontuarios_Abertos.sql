034a49d92976ba90bd3799603c42646aa1b17f35
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2025-01-29 09:41:49 -0300

    Update Prontuarios_Abertos.sql

--- SQL_MvSoul_Mv2000/Prontuarios_Abertos.sql
+++ SQL_MvSoul_Mv2000/Prontuarios_Abertos.sql
@@ -7,7 +7,6 @@ FROM
          same same
     INNER JOIN paciente ON paciente.cd_paciente = same.cd_paciente
     INNER JOIN it_same ON same.nr_matricula_same = it_same.nr_matricula_same
-    INNER JOIN atendime ON it_same.cd_atendimento = atendime.cd_atendimento
 WHERE
         same.nr_matricula_same >= 107001
     AND same.dt_cadastro BETWEEN TO_DATE('29/01/2025', 'DD/MM/YYYY') AND TO_DATE('29/01/2025', 'DD/MM/YYYY')
