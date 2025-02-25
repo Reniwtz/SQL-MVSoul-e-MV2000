0948d80909d9738ed183e020cb01c1e217e72059
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2025-02-25 17:53:17 -0300

    Update Alteracao_de_Plano_Contabil.sql

--- SQL_MvSoul_Mv2000/Alteracao_de_Plano_Contabil.sql
+++ SQL_MvSoul_Mv2000/Alteracao_de_Plano_Contabil.sql
@@ -4,11 +4,8 @@ SELECT * FROM update dbamv.plano_estr set tp_conta = 'S' where cd_reduzido = 57
   
 SELECT * FROM plano_contas where cd_contabil = '4.1.02.02'
 
-
-
+  
 SELECT Max(cd_reduzido)+1 FROM plano_contas
-
-
 
 
 ALTER SEQUENCE seq_plano_contas  INCREMENT BY 28847;
@@ -16,8 +13,12 @@ SELECT seq_plano_contas.NEXTVAL FROM dual;
 ALTER SEQUENCE seq_plano_contas INCREMENT BY 1;
 
 
-
 SELECT * FROM plano_estr WHERE cd_reduzido = 29007
 
 UPDATE plano_estr SET TP_CONTA  = 'S' WHERE cd_reduzido = 29007
+
+  
+update plano_contas
+SET tp_natureza = 'D'
+WHERE CD_REDUZIDO = 10181
 
