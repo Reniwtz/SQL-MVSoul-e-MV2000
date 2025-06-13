3b6f2d1e06dcd6b45cee10b72e4de0af3996b7dd
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2025-06-13 08:08:26 -0300

    Update Usuários_e_Papeis.sql

--- SQL_MvSoul_Mv2000/Usuários_e_Papeis.sql
+++ SQL_MvSoul_Mv2000/Usuários_e_Papeis.sql
@@ -1,17 +1,24 @@
---Papel Gerencial
 SELECT
     usuarios.cd_usuario,
     usuarios.nm_usuario,
-    usuarios.cd_prestador, 
+    usuarios.cd_prestador,
     papel_usuarios.cd_papel,
     papel.ds_papel
 FROM
          usuarios usuarios
     INNER JOIN papel_usuarios ON usuarios.cd_usuario = papel_usuarios.cd_usuario
     INNER JOIN papel ON papel.cd_papel = papel_usuarios.cd_papel
-    --INNER JOIN papel_mod ON papel_mod.cd_papel = papel.cd_papel
+    INNER JOIN papel_mod ON papel_mod.cd_papel = papel.cd_papel
 WHERE
-        usuarios.sn_ativo LIKE 'S'
+    usuarios.sn_ativo LIKE 'S'
     AND usuarios.tp_privilegio LIKE 'U'
-    AND papel.ds_papel like '%GERENCIAL%'
-    --AND papel_mod.cd_modulo = 'CON_ATE'
+    --AND papel.cd_papel like  '29'
+    AND papel_mod.cd_modulo = 'C_ORDCOM'
+    --AND papel.ds_papel like '%GERENCIAL%'
+    AND usuarios.sn_ativo LIKE 'S'
+GROUP BY
+    usuarios.cd_usuario,
+    usuarios.nm_usuario,
+    usuarios.cd_prestador,
+    papel_usuarios.cd_papel,
+    papel.ds_papel
