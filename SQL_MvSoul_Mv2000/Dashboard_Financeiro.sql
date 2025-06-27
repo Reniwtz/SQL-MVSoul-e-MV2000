9917f0871755768ced05c1e39ddc166af6f95c24
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2025-06-27 09:48:31 -0300

    Update Dashboard_Financeiro.sql

--- SQL_MvSoul_Mv2000/Dashboard_Financeiro.sql
+++ SQL_MvSoul_Mv2000/Dashboard_Financeiro.sql
@@ -17,3 +17,23 @@ FROM
 WHERE
     cd_reduzido LIKE '2915'
     AND dt_emissao BETWEEN TO_DATE('10/06/2025', 'DD/MM/YYYY') AND TO_DATE('10/06/2025', 'DD/MM/YYYY');
+
+
+
+--Convênio
+SELECT
+    *
+FROM
+    con_rec con_rec
+WHERE
+    cd_reduzido IN ( '1302', '1303', '1305', '1306', '1307',
+                     '1308', '1311', '1313', '1314', '1315',
+                     '1316', '1317', '1318', '1319', '1323',
+                     '1324', '1325', '1326', '1328', '1332',
+                     '1333', '1336', '1341', '1346', '1424' )
+
+
+select * from reccon_rec where cd_itcon_rec like '138815'
+
+select * from con_rec where cd_con_rec like '138911'
+select * from itcon_rec where cd_con_rec like '138911'
