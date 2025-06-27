e11ba8bec7f7779f2ffd424e96be98cb8afa94e7
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2025-06-27 10:15:37 -0300

    Update Dashboard_Financeiro.sql

--- SQL_MvSoul_Mv2000/Dashboard_Financeiro.sql
+++ SQL_MvSoul_Mv2000/Dashboard_Financeiro.sql
@@ -22,15 +22,32 @@ WHERE
 
 --Convênio
 SELECT
-    *
+    con_rec.dt_emissao                      AS competencia,
+    reccon_rec.vl_recebido                  AS valor_recebido,
+    con_rec.cd_reduzido,
+    decode(con_rec.cd_reduzido, '1302', 'UNIMED', '1303', 'ASSEFAZ',
+           '1305', 'FACENE BAYEUX / VALENTINA / HUNE', '1306', 'POSTAL SAÚDE', '1307',
+           'CAMED', '1308', 'FUNCEF', '1311', 'FUNASA',
+           '1313', 'AFRAFEP', '1314', 'GEAP', '1315',
+           'CAPESAÚDE', '1316', 'AMI SAÚDE', '1317', 'PETROBRAS',
+           '1318', 'SUL AMÉRICA', '1319', 'CASSI', '1323',
+           'COMSEDER', '1324', 'BRADESCO SAÚDE / OPERADORA', '1325', 'AMIL',
+           '1326', 'MEDSERVICE', '1328', 'HAPVIDA', '1332',
+           'FUSMA', '1333', 'GAMA', '1336', 'FCA',
+           '1341', 'PREFEITURAS', '1346', 'ASTRAZENECA', '1424',
+           'UNIMED CEDAPP', 'DESCONHECIDO' -- valor default
+           ) AS convenio
 FROM
-    con_rec con_rec
+         con_rec
+    INNER JOIN itcon_rec ON itcon_rec.cd_con_rec = con_rec.cd_con_rec
+    INNER JOIN reccon_rec ON reccon_rec.cd_itcon_rec = itcon_rec.cd_itcon_rec
 WHERE
-    cd_reduzido IN ( '1302', '1303', '1305', '1306', '1307',
-                     '1308', '1311', '1313', '1314', '1315',
-                     '1316', '1317', '1318', '1319', '1323',
-                     '1324', '1325', '1326', '1328', '1332',
-                     '1333', '1336', '1341', '1346', '1424' )
+    con_rec.cd_con_rec LIKE '138911'
+    AND con_rec.cd_reduzido IN ( '1302', '1303', '1305', '1306', '1307',
+                                 '1308', '1311', '1313', '1314', '1315',
+                                 '1316', '1317', '1318', '1319', '1323',
+                                 '1324', '1325', '1326', '1328', '1332',
+                                 '1333', '1336', '1341', '1346', '1424' );
 
 
 select * from reccon_rec where cd_itcon_rec like '138815'
