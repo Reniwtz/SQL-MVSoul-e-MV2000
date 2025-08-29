1dcaece9558889ecc66b7eb3314fc522ada41d08
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2025-08-29 11:36:44 -0300

    Update campos_do_documento.sql

--- SQL_MvSoul_Mv2000/campos_do_documento.sql
+++ SQL_MvSoul_Mv2000/campos_do_documento.sql
@@ -1,4 +1,30 @@
 SELECT
+    pw_documento_clinico.cd_documento_clinico,
+    pw_editor_clinico.cd_editor_registro,
+    pw_documento_clinico.cd_paciente    AS cad,
+    paciente.nm_paciente                AS nome_do_paciente,
+    pw_documento_clinico.cd_atendimento AS atendimento,
+    pw_documento_clinico.nm_documento   AS tipo_de_documento
+FROM
+         pw_documento_clinico pw_documento_clinico
+    INNER JOIN pw_editor_clinico ON pw_editor_clinico.cd_documento_clinico = pw_documento_clinico.cd_documento_clinico
+    INNER JOIN paciente ON paciente.cd_paciente = pw_documento_clinico.cd_paciente
+WHERE
+    pw_documento_clinico.cd_objeto LIKE '105' --solicitações
+    AND pw_documento_clinico.tp_status LIKE '%FECHADO%'
+    AND pw_documento_clinico.dh_referencia BETWEEN TO_DATE('01/08/2025', 'DD/MM/YYYY') AND TO_DATE('31/08/2025', 'DD/MM/YYYY')
+    AND pw_editor_clinico.cd_documento IN ( '228', '370' ) --apac
+GROUP BY
+    pw_documento_clinico.cd_documento_clinico,
+    pw_editor_clinico.cd_editor_registro,
+    pw_documento_clinico.cd_paciente,
+    paciente.nm_paciente,
+    pw_documento_clinico.cd_atendimento,
+    pw_documento_clinico.nm_documento
+
+    
+----------------------------------------------------------------------------------------------------------------------
+SELECT
     dbms_lob.substr(erc.lo_valor, 4000) AS ds_resposta
 FROM
     dbamv.pw_documento_clinico  pdc,
@@ -27,7 +53,7 @@ WHERE
     );
 
 
-
+---------------------------------------------------------------------------------------
 SELECT
     procedimento_sus.cd_procedimento,
     procedimento_sus.cd_procedimento
