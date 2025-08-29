91d71e6da4a891fbcdcf77b607286b16c0178a75
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2025-08-29 13:33:50 -0300

    Update campos_do_documento.sql

--- SQL_MvSoul_Mv2000/campos_do_documento.sql
+++ SQL_MvSoul_Mv2000/campos_do_documento.sql
@@ -1,26 +1,40 @@
 SELECT
-    pw_documento_clinico.cd_documento_clinico,
-    pw_editor_clinico.cd_editor_registro,
-    pw_documento_clinico.cd_paciente    AS cad,
-    paciente.nm_paciente                AS nome_do_paciente,
-    pw_documento_clinico.cd_atendimento AS atendimento,
-    pw_documento_clinico.nm_documento   AS tipo_de_documento
+    pw_documento_clinico.cd_atendimento,
+    MAX(
+        CASE
+            WHEN editor_campo.ds_identificador = 'TX_NOMPRIN_AUTOPA_1' THEN
+                dbms_lob.substr(editor_registro_campo.lo_valor, 4000)
+        END
+    ) AS tx_nomprim_autopa_1,
+    MAX(
+        CASE
+            WHEN editor_campo.ds_identificador = 'CB_NOMESEC_AUTOPA_1' THEN
+                dbms_lob.substr(editor_registro_campo.lo_valor, 4000)
+        END
+    ) AS cb_nomsec_autopa_1
 FROM
          pw_documento_clinico pw_documento_clinico
-    INNER JOIN pw_editor_clinico ON pw_editor_clinico.cd_documento_clinico = pw_documento_clinico.cd_documento_clinico
-    INNER JOIN paciente ON paciente.cd_paciente = pw_documento_clinico.cd_paciente
+    JOIN pw_editor_clinico ON pw_editor_clinico.cd_documento_clinico = pw_documento_clinico.cd_documento_clinico
+    JOIN editor_registro_campo ON editor_registro_campo.cd_registro = pw_editor_clinico.cd_editor_registro
+    JOIN editor_campo ON editor_campo.cd_campo = editor_registro_campo.cd_campo
 WHERE
-    pw_documento_clinico.cd_objeto LIKE '105' --solicitações
-    AND pw_documento_clinico.tp_status LIKE '%FECHADO%'
-    AND pw_documento_clinico.dh_referencia BETWEEN TO_DATE('01/08/2025', 'DD/MM/YYYY') AND TO_DATE('31/08/2025', 'DD/MM/YYYY')
-    AND pw_editor_clinico.cd_documento IN ( '228', '370' ) --apac
+        pw_documento_clinico.tp_status = 'FECHADO'
+    AND pw_documento_clinico.cd_atendimento = '4194846'
+    AND pw_editor_clinico.cd_documento IN ( 370 )
+    AND editor_campo.ds_identificador IN ( 'TX_NOMPRIN_AUTOPA_1', 'CB_NOMESEC_AUTOPA_1' )
+    AND pw_documento_clinico.cd_documento_clinico = (
+        SELECT
+            MAX(pw_documento_clinico.cd_documento_clinico)
+        FROM
+                 pw_documento_clinico pw_documento_clinico
+            JOIN pw_editor_clinico pw_editor_clinico ON pw_editor_clinico.cd_documento_clinico = pw_documento_clinico.cd_documento_clinico
+        WHERE
+                pw_documento_clinico.tp_status = 'FECHADO'
+            AND pw_documento_clinico.cd_atendimento = '4194846'
+            AND pw_editor_clinico.cd_documento IN ( 370 )
+    )
 GROUP BY
-    pw_documento_clinico.cd_documento_clinico,
-    pw_editor_clinico.cd_editor_registro,
-    pw_documento_clinico.cd_paciente,
-    paciente.nm_paciente,
-    pw_documento_clinico.cd_atendimento,
-    pw_documento_clinico.nm_documento
+    pw_documento_clinico.cd_atendimento;
 
     
 ----------------------------------------------------------------------------------------------------------------------
