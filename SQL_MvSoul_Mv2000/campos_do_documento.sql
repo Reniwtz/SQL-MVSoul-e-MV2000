706e89b5f83be286b5a9b6398108fef5ba10403e
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2025-08-29 12:54:47 -0300

    Update campos_do_documento.sql

--- SQL_MvSoul_Mv2000/campos_do_documento.sql
+++ SQL_MvSoul_Mv2000/campos_do_documento.sql
@@ -25,32 +25,42 @@ GROUP BY
     
 ----------------------------------------------------------------------------------------------------------------------
 SELECT
-    dbms_lob.substr(erc.lo_valor, 4000) AS ds_resposta
+    pdc.cd_atendimento,
+    MAX(CASE
+        WHEN ecp.ds_identificador = 'TX_NOMPRIN_AUTOPA_1'
+        THEN DBMS_LOB.SUBSTR(erc.lo_valor, 4000)
+    END) AS tx_nomprim_autopa_1,
+
+    MAX(CASE
+        WHEN ecp.ds_identificador = 'CB_NOMESEC_AUTOPA_1'
+        THEN DBMS_LOB.SUBSTR(erc.lo_valor, 4000)
+    END) AS cb_nomsec_autopa_1
+
 FROM
-    dbamv.pw_documento_clinico  pdc,
-    dbamv.pw_editor_clinico     pec,
-    dbamv.editor_registro_campo erc,
-    dbamv.editor_campo          ecp
+    dbamv.pw_documento_clinico  pdc
+    JOIN dbamv.pw_editor_clinico     pec ON pec.cd_documento_clinico = pdc.cd_documento_clinico
+    JOIN dbamv.editor_registro_campo erc ON erc.cd_registro = pec.cd_editor_registro
+    JOIN dbamv.editor_campo          ecp ON ecp.cd_campo = erc.cd_campo
+
 WHERE
-        pec.cd_documento_clinico = pdc.cd_documento_clinico
-    AND erc.cd_registro = pec.cd_editor_registro
-    AND ecp.cd_campo = erc.cd_campo
-    AND pdc.tp_status IN ( 'FECHADO' )
-    AND pdc.cd_atendimento = 4152614
-    AND pec.cd_documento IN ( 370 )
-    AND ecp.ds_identificador IN ( 'CB_NOMESEC_AUTOPA_1' )
+    pdc.tp_status = 'FECHADO'
+    AND pdc.cd_atendimento = '4194846'
+    AND pec.cd_documento IN (370)
+    AND ecp.ds_identificador IN (
+        'TX_NOMPRIN_AUTOPA_1',
+        'CB_NOMESEC_AUTOPA_1'
+    )
     AND pdc.cd_documento_clinico = (
-        SELECT
-            MAX(pdc.cd_documento_clinico)
-        FROM
-            dbamv.pw_documento_clinico pdc,
-            dbamv.pw_editor_clinico    pec
-        WHERE
-                pec.cd_documento_clinico = pdc.cd_documento_clinico
-            AND pdc.tp_status IN ( 'FECHADO' )
-            AND pdc.cd_atendimento = 4152614
-            AND pec.cd_documento IN ( 370 )
-    );
+        SELECT MAX(pdc.cd_documento_clinico)
+        FROM dbamv.pw_documento_clinico pdc
+        JOIN dbamv.pw_editor_clinico pec ON pec.cd_documento_clinico = pdc.cd_documento_clinico
+        WHERE pdc.tp_status = 'FECHADO'
+          AND pdc.cd_atendimento = '4194846'
+          AND pec.cd_documento IN (370)
+    )
+GROUP BY
+    pdc.cd_atendimento;
+    
 
 
 ---------------------------------------------------------------------------------------
