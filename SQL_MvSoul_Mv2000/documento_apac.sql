dbee39605c64d71e973617c12f44f8b0ebdfb688
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2025-09-01 12:16:59 -0300

    Create documento_apac.sql

---
+++ SQL_MvSoul_Mv2000/documento_apac.sql
@@ -0,0 +1,46 @@
+WITH base_dados AS (
+    SELECT
+        pw_documento_clinico.cd_paciente AS cad,
+        paciente.nm_paciente AS nome_do_paciente,
+        pw_documento_clinico.cd_atendimento AS atendimento,
+        pw_documento_clinico.nm_documento AS tipo_de_documento,
+        MAX(
+            CASE
+                WHEN editor_campo.ds_identificador = 'TX_NOMPRIN_AUTOPA_1' THEN
+                    SUBSTR(DBMS_LOB.SUBSTR(editor_registro_campo.lo_valor, 4000), 1, 10)
+            END
+        ) AS procedimento
+    FROM
+        pw_documento_clinico
+        INNER JOIN pw_editor_clinico ON pw_editor_clinico.cd_documento_clinico = pw_documento_clinico.cd_documento_clinico
+        INNER JOIN editor_registro_campo ON editor_registro_campo.cd_registro = pw_editor_clinico.cd_editor_registro
+        INNER JOIN editor_campo ON editor_campo.cd_campo = editor_registro_campo.cd_campo
+        INNER JOIN paciente ON paciente.cd_paciente = pw_documento_clinico.cd_paciente
+    WHERE
+        pw_documento_clinico.tp_status = 'FECHADO'
+        AND pw_editor_clinico.cd_documento IN (370)
+        AND editor_campo.ds_identificador IN ('TX_NOMPRIN_AUTOPA_1', 'CB_NOMESEC_AUTOPA_1')
+        AND pw_documento_clinico.dh_referencia BETWEEN TO_DATE('01/08/2025', 'DD/MM/YYYY') AND TO_DATE('31/08/2025', 'DD/MM/YYYY')
+    GROUP BY
+        pw_documento_clinico.cd_paciente,
+        paciente.nm_paciente,
+        pw_documento_clinico.cd_atendimento,
+        pw_documento_clinico.nm_documento
+)
+
+SELECT
+    bd.*,
+    tos.qt_fisico AS quantidade_total,
+    psus.ds_procedimento,
+    ROUND(tos.vl_orcamento / NULLIF(tos.qt_fisico, 0), 2) AS valor_unitario,
+    tos.vl_orcamento AS valor_total
+FROM
+    base_dados bd
+LEFT JOIN (
+    SELECT *
+    FROM teto_orcamentario_proced_sus
+    WHERE cd_fat_sia = (
+        SELECT MAX(cd_fat_sia) FROM teto_orcamentario_proced_sus
+    )
+) tos ON tos.cd_procedimento = bd.procedimento
+LEFT JOIN procedimento_sus psus ON psus.cd_procedimento = bd.procedimento
