776884a5b55be110f452e76467c622571d191119
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2025-09-25 15:29:44 -0300

    Update relatorio_ agendamento_oncologico.sql

--- SQL_MvSoul_Mv2000/relatorio_ agendamento_oncologico.sql
+++ SQL_MvSoul_Mv2000/relatorio_ agendamento_oncologico.sql
@@ -19,3 +19,30 @@ GROUP BY
     paciente.cd_paciente,
     paciente.nm_paciente,
     tratamento.nm_protocolo
+
+--------------------------------------------------------------------------------
+SELECT
+    atendime.cd_paciente,
+    atendime.dt_atendimento
+FROM
+    atendime atendime
+WHERE
+        atendime.sn_atendimento_apac = 'S'
+    AND atendime.dt_atendimento BETWEEN TO_DATE('01/09/2025', 'DD/MM/YYYY') AND TO_DATE('30/09/2025', 'DD/MM/YYYY')
+    AND atendime.cd_paciente IN (
+        SELECT
+            paciente.cd_paciente AS cad
+        FROM
+                 solic_agendamento
+            INNER JOIN atendime ON solic_agendamento.cd_atendimento = atendime.cd_atendimento
+            INNER JOIN paciente ON paciente.cd_paciente = atendime.cd_paciente
+        WHERE
+            solic_agendamento.dt_referencia BETWEEN TO_DATE('01/09/2025', 'DD/MM/YYYY') AND TO_DATE('30/09/2025', 'DD/MM/YYYY')
+            AND solic_agendamento.tp_situacao LIKE 'A'
+            AND solic_agendamento.tp_agendamento LIKE '%QUI%'
+        GROUP BY
+            paciente.cd_paciente
+    )
+GROUP BY
+    atendime.cd_paciente,
+    atendime.dt_atendimento
