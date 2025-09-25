43f39b8c096a1e02c8354ffd0904741ac7ccfef1
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2025-09-25 16:16:29 -0300

    Update relatorio_ agendamento_oncologico.sql

--- SQL_MvSoul_Mv2000/relatorio_ agendamento_oncologico.sql
+++ SQL_MvSoul_Mv2000/relatorio_ agendamento_oncologico.sql
@@ -46,3 +46,47 @@ WHERE
 GROUP BY
     atendime.cd_paciente,
     atendime.dt_atendimento
+
+--------------------------------------------------------------------------------
+SELECT
+    atendime.cd_procedimento                                                                AS codigo_do_procedimento,
+    procedimento_sus.ds_procedimento                                                        AS descricao_do_procedimento,
+    to_char(atendime.dt_atendimento, 'DD/MM/YYYY')                                          AS data,
+    SUM(teto_orcamentario_proced_sus.vl_orcamento / teto_orcamentario_proced_sus.qt_fisico) AS total
+FROM
+         atendime
+    INNER JOIN paciente ON paciente.cd_paciente = atendime.cd_paciente
+    INNER JOIN procedimento_sus ON atendime.cd_procedimento = procedimento_sus.cd_procedimento
+    LEFT JOIN (
+        SELECT
+            t1.*
+        FROM
+            teto_orcamentario_proced_sus t1
+        WHERE
+            t1.cd_fat_sia = (
+                SELECT
+                    MAX(cd_fat_sia)
+                FROM
+                    teto_orcamentario_proced_sus
+            )
+    ) teto_orcamentario_proced_sus ON teto_orcamentario_proced_sus.cd_procedimento = procedimento_sus.cd_procedimento
+WHERE
+        atendime.sn_atendimento_apac = 'S'
+    AND cd_atendimento IN (
+        SELECT
+            solic_agendamento.cd_atendimento
+        FROM
+            solic_agendamento
+        WHERE
+            solic_agendamento.dt_referencia BETWEEN TO_DATE('01/09/2025', 'DD/MM/YYYY') AND TO_DATE('30/09/2025', 'DD/MM/YYYY')
+            AND solic_agendamento.tp_situacao LIKE 'A'
+            AND solic_agendamento.tp_agendamento LIKE '%QUI%'
+        GROUP BY
+            solic_agendamento.cd_atendimento
+    )
+GROUP BY
+    atendime.cd_procedimento,
+    procedimento_sus.ds_procedimento,
+    to_char(atendime.dt_atendimento, 'DD/MM/YYYY')
+ORDER BY
+    data
