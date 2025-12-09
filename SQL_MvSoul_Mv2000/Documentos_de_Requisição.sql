baee83f7970b731b999f057f24bca8c496e4d3c7
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2025-12-09 17:19:11 -0300

    Update Documentos_de_Requisição.sql

--- SQL_MvSoul_Mv2000/Documentos_de_Requisição.sql
+++ SQL_MvSoul_Mv2000/Documentos_de_Requisição.sql
@@ -31,27 +31,35 @@ SELECT
     paciente.cd_paciente           AS cad,
     pre_med.hr_pre_med             AS hora_da_solicitação,
     pre_med.nm_usuario_autorizador AS solicitante,
-    tip_presc.ds_tip_presc         AS exame_solicitado
+    tip_presc.ds_tip_presc         AS exame_solicitado,
+    vw_res_exames_pssd.cd_ped_lab  AS pedido,
+    vw_res_exames_pssd.dt_laudo    AS data_do_laudo
 FROM
          pre_med pre_med
     INNER JOIN itpre_med ON itpre_med.cd_pre_med = pre_med.cd_pre_med
     INNER JOIN atendime ON atendime.cd_atendimento = pre_med.cd_atendimento
     INNER JOIN paciente ON paciente.cd_paciente = atendime.cd_paciente
     INNER JOIN tip_presc ON tip_presc.cd_tip_presc = itpre_med.cd_tip_presc
+    INNER JOIN vw_res_exames_pssd ON vw_res_exames_pssd.cd_atendimento = atendime.cd_atendimento
 WHERE
     pre_med.cd_objeto LIKE '84'
     AND paciente.cd_paciente LIKE '123616'
-    AND pre_med.dt_pre_med BETWEEN TO_DATE('07/12/2025', 'DD/MM/YYYY') AND TO_DATE('07/12/2025', 'DD/MM/YYYY')
+    AND pre_med.dt_pre_med BETWEEN TO_DATE('01/10/2025', 'DD/MM/YYYY') AND TO_DATE('07/12/2025', 'DD/MM/YYYY')
     AND itpre_med.cd_tip_esq LIKE 'EXL'
+    AND vw_res_exames_pssd.hr_ped_lab = pre_med.hr_pre_med
 GROUP BY
     atendime.cd_atendimento,
     paciente.cd_paciente,
     pre_med.hr_pre_med,
     pre_med.nm_usuario_autorizador,
-    tip_presc.ds_tip_presc;
+    tip_presc.ds_tip_presc,
+    vw_res_exames_pssd.cd_ped_lab,
+    vw_res_exames_pssd.dt_laudo
+ORDER BY
+    atendime.cd_atendimento,
+    vw_res_exames_pssd.cd_ped_lab;
 
 --------------------------------------------------------------------------------    
---REQUISIÇÃO DE EXAME DE IMAGEM NA PRESCRIÇÃO
 --REQUISIÇÃO DE EXAME DE IMAGEM NA PRESCRIÇÃO
 SELECT
     atendime.cd_atendimento        AS atendimento,
