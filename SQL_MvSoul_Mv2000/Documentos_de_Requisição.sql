505bf589539a8376a33509074c26786ec18fe7da
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2025-12-24 10:30:51 -0300

    Update Documentos_de_Requisição.sql

--- SQL_MvSoul_Mv2000/Documentos_de_Requisição.sql
+++ SQL_MvSoul_Mv2000/Documentos_de_Requisição.sql
@@ -26,93 +26,27 @@ GROUP BY
 
 --------------------------------------------------------------------------------
 --REQUISIÇÃO DE EXAME DE SANGUE NA PRESCRIÇÃO
-WITH
--- 1) PRIMEIRA CONSULTA (SOLICITAÇÕES DOS EXAMES) + NUMERAÇÃO
-solicitacoes AS (
-    SELECT
-        solicitacoes_inner.atendimento,
-        solicitacoes_inner.cad,
-        solicitacoes_inner.hora_da_solicitacao,
-        solicitacoes_inner.solicitante,
-        solicitacoes_inner.exame_solicitado,
-        solicitacoes_inner.pedido,
-        solicitacoes_inner.unidade_de_internacao,
-        ROW_NUMBER() OVER (
-            ORDER BY
-                solicitacoes_inner.atendimento,
-                solicitacoes_inner.pedido,
-                solicitacoes_inner.exame_solicitado
-        ) AS rownumber
-    FROM (
-        SELECT DISTINCT
-            atendime.cd_atendimento        AS atendimento,
-            paciente.cd_paciente           AS cad,
-            pre_med.hr_pre_med             AS hora_da_solicitacao,
-            pre_med.nm_usuario_autorizador AS solicitante,
-            tip_presc.ds_tip_presc         AS exame_solicitado,
-            vw_res_exames_pssd.cd_ped_lab  AS pedido,
-            unid_int.ds_unid_int           AS unidade_de_internacao
-        FROM
-                 pre_med pre_med
-            INNER JOIN itpre_med
-                    ON itpre_med.cd_pre_med = pre_med.cd_pre_med
-            INNER JOIN atendime
-                    ON atendime.cd_atendimento = pre_med.cd_atendimento
-            INNER JOIN paciente
-                    ON paciente.cd_paciente = atendime.cd_paciente
-            INNER JOIN tip_presc
-                    ON tip_presc.cd_tip_presc = itpre_med.cd_tip_presc
-            INNER JOIN vw_res_exames_pssd
-                    ON vw_res_exames_pssd.cd_atendimento = atendime.cd_atendimento
-                   AND vw_res_exames_pssd.hr_ped_lab    = pre_med.hr_pre_med
-            INNER JOIN unid_int
-                    ON unid_int.cd_unid_int = pre_med.cd_unid_int
-        WHERE
-            pre_med.cd_objeto = '84'
-            AND atendime.cd_atendimento = '4288216'
-            AND pre_med.dt_pre_med BETWEEN
-                TO_DATE('01/10/2025', 'DD/MM/YYYY') AND TO_DATE('31/12/2025', 'DD/MM/YYYY')
-            AND itpre_med.cd_tip_esq = 'EXL'
-    ) solicitacoes_inner
-),
-
--- 2) SEGUNDA CONSULTA (LAUDOS DOS EXAMES) + NUMERAÇÃO
-laudos AS (
-    SELECT
-        vw_res_exames_pssd.cd_atendimento,
-        vw_res_exames_pssd.cd_ped_lab,
-        vw_res_exames_pssd.dt_laudo,
-        vw_res_exames_pssd.nm_exa_lab,
-        ROW_NUMBER() OVER (
-            ORDER BY
-                vw_res_exames_pssd.cd_atendimento,
-                vw_res_exames_pssd.cd_ped_lab,
-                vw_res_exames_pssd.nm_exa_lab
-        ) AS rownumber
-    FROM vw_res_exames_pssd vw_res_exames_pssd
-    WHERE
-        vw_res_exames_pssd.cd_atendimento = '4288216'
-)
-
--- 3) JUNTANDO LINHA A LINHA
 SELECT
-    solicitacoes.atendimento,
-    solicitacoes.cad,
-    solicitacoes.hora_da_solicitacao,
-    solicitacoes.solicitante,
-    solicitacoes.exame_solicitado,
-    solicitacoes.pedido,
-    solicitacoes.unidade_de_internacao,
-    laudos.dt_laudo,
-    laudos.nm_exa_lab
+    vw_res_exames_pssd.cd_atendimento AS atendimento,
+    vw_res_exames_pssd.cd_paciente    AS cad,
+    vw_res_exames_pssd.hr_ped_lab     AS hora_da_solicitação,
+    prestador.nm_prestador            AS solicitante,
+    vw_res_exames_pssd.nm_exa_lab     AS exame_solicitado,
+    vw_res_exames_pssd.cd_ped_lab     AS pedido,
+    unid_int.ds_unid_int              AS unidade_de_internacao,
+    vw_res_exames_pssd.hr_laudo       AS hora_do_laudo
 FROM
-    solicitacoes solicitacoes
-    JOIN laudos laudos
-        ON laudos.rownumber = solicitacoes.rownumber
+    vw_res_exames_pssd vw_res_exames_pssd
+    LEFT JOIN leito ON leito.cd_leito = vw_res_exames_pssd.cd_leito
+    LEFT JOIN unid_int ON unid_int.cd_unid_int = leito.cd_unid_int
+    INNER JOIN ped_lab ON ped_lab.cd_ped_lab = vw_res_exames_pssd.cd_ped_lab
+    INNER JOIN prestador ON ped_lab.cd_prestador = prestador.cd_prestador
+WHERE
+    vw_res_exames_pssd.cd_atendimento LIKE '4267381'
+    --vw_res_exames_pssd.hr_ped_lab BETWEEN TO_DATE('10/12/2025', 'DD/MM/YYYY') AND TO_DATE('23/12/2025', 'DD/MM/YYYY')
 ORDER BY
-    solicitacoes.atendimento,
-    solicitacoes.pedido,
-    solicitacoes.exame_solicitado;
+    vw_res_exames_pssd.cd_atendimento,
+    vw_res_exames_pssd.cd_ped_lab
 
 
 
