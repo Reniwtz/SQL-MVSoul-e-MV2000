cf1662d08d9d8c10beb67f5f72db7af66d1813a8
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2026-01-08 16:28:57 -0300

    Create movimentação_bancária.sql

---
+++ SQL_MvSoul_Mv2000/movimentação_bancária.sql
@@ -0,0 +1,58 @@
+SELECT
+    banco.nm_banco             AS nome_do_banco,
+    con_cor.ds_con_cor         AS descrição_da_conta,
+    con_cor.nr_conta           AS número_da_conta,
+    mov_concor.ds_movimentacao AS descrição_da_movimentação,
+    mov_concor.vl_movimentacao AS valor_da_movimentação,
+    mov_concor.dt_movimentacao AS data_da_movimentação,
+    CASE
+        WHEN lan_concor.tp_operacao_saldo_conta = '+' THEN 'Crédito'
+        WHEN lan_concor.tp_operacao_saldo_conta = '-' THEN 'Débito'
+        ELSE 'Indefinido'
+    END AS tipo_operacao 
+FROM
+         con_cor con_cor
+    INNER JOIN banco ON banco.cd_banco = con_cor.cd_banco
+    INNER JOIN mov_concor ON mov_concor.cd_con_cor = con_cor.cd_con_cor
+    INNER JOIN lan_concor ON lan_concor.cd_lan_concor = mov_concor.cd_lan_concor
+WHERE
+    con_cor.sn_ativo LIKE 'S'
+    AND mov_concor.dt_movimentacao BETWEEN TO_DATE('01/01/2026', 'DD/MM/YYYY') AND TO_DATE('08/01/2026', 'DD/MM/YYYY')
+ORDER BY
+    banco.nm_banco,
+    con_cor.ds_con_cor;
+--------------------------------------------------------------------------------
+
+SELECT
+    --banco.nm_banco                  AS nome_do_banco,
+    --con_cor.ds_con_cor              AS descrição_da_conta,
+    CASE
+        WHEN lan_concor.tp_operacao_saldo_conta = '+' THEN
+            'Crédito'
+        WHEN lan_concor.tp_operacao_saldo_conta = '-' THEN
+            'Débito'
+        ELSE
+            'Indefinido'
+    END                             AS tipo_operacao,
+    SUM(mov_concor.vl_movimentacao) AS total_movimentado
+FROM
+         con_cor
+    INNER JOIN banco ON banco.cd_banco = con_cor.cd_banco
+    INNER JOIN mov_concor ON mov_concor.cd_con_cor = con_cor.cd_con_cor
+    INNER JOIN lan_concor ON lan_concor.cd_lan_concor = mov_concor.cd_lan_concor
+WHERE
+        con_cor.sn_ativo = 'S'
+    AND mov_concor.dt_movimentacao BETWEEN TO_DATE('01/01/2026', 'DD/MM/YYYY') AND TO_DATE('08/01/2026', 'DD/MM/YYYY')
+GROUP BY
+    --banco.nm_banco,
+    --con_cor.ds_con_cor,
+    CASE
+            WHEN lan_concor.tp_operacao_saldo_conta = '+' THEN
+                'Crédito'
+            WHEN lan_concor.tp_operacao_saldo_conta = '-' THEN
+                'Débito'
+            ELSE
+                'Indefinido'
+    END
+ORDER BY
+    tipo_operacao;
