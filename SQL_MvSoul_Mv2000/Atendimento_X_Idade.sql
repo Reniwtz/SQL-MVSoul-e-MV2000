8dacd4d6f08c3cb68e935c7e641be5c381d1ac66
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2026-01-13 14:15:22 -0300

    Update Atendimento_X_Idade.sql

--- SQL_MvSoul_Mv2000/Atendimento_X_Idade.sql
+++ SQL_MvSoul_Mv2000/Atendimento_X_Idade.sql
@@ -35,3 +35,33 @@ GROUP BY
     paciente.dt_nascimento
 ORDER BY
     idade DESC;
+
+
+
+
+-- Atendimento por CIDxAtendimentoXIdade
+SELECT
+    atendime.cd_paciente,
+    paciente.nm_paciente,
+    atendime.cd_cid,
+    cid.ds_cid,
+    same.nr_matricula_same,
+    TRUNC(MONTHS_BETWEEN(atendime.dt_atendimento, paciente.dt_nascimento) / 12) AS idade_na_data_atendimento 
+FROM
+         atendime atendime
+    INNER JOIN cid ON cid.cd_cid = atendime.cd_cid
+    INNER JOIN paciente ON paciente.cd_paciente = atendime.cd_paciente
+    LEFT JOIN same ON same.cd_paciente = paciente.cd_paciente
+WHERE
+    atendime.dt_atendimento BETWEEN TO_DATE('01/01/24', 'DD/MM/YY') AND TO_DATE('31/12/24', 'DD/MM/YY')
+    AND atendime.cd_cid LIKE 'C50%'
+    AND TRUNC(MONTHS_BETWEEN(atendime.dt_atendimento, paciente.dt_nascimento) / 12) BETWEEN 35 AND 49
+GROUP BY
+    atendime.cd_paciente,
+    paciente.nm_paciente,
+    atendime.cd_cid,
+    cid.ds_cid,
+    same.nr_matricula_same,
+    TRUNC(MONTHS_BETWEEN(atendime.dt_atendimento, paciente.dt_nascimento) / 12)
+ORDER BY
+    atendime.cd_cid
