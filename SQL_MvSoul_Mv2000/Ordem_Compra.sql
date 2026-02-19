8af1886fe5f6e620b1037ed39a05049822673ce3
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2026-02-19 14:29:42 -0300

    Add detailed report query for purchase orders
    Added a report query for purchase orders with detailed information including authorization and product details.

--- SQL_MvSoul_Mv2000/Ordem_Compra.sql
+++ SQL_MvSoul_Mv2000/Ordem_Compra.sql
@@ -46,7 +46,7 @@ ORDER BY
     sol_com.cd_sol_com DESC,
     sol_com.cd_sol_com
 
-
+----------------------------------------------------------------------------------
 -- Ultimo aprovador
 SELECT
     *
@@ -57,3 +57,62 @@ WHERE
 ORDER BY
     dt_autorizacao DESC
 FETCH FIRST 1 ROW ONLY;
+
+--------------------------------------------------------------------------------
+-- Relatório do Gerente
+SELECT
+    sol_com.cd_sol_com                                           AS código_solicitação,
+    sol_com.dt_sol_com                                           AS data_da_solicitação,
+    sol_com.cd_usuario                                           AS usuário_solicitante,
+    ord_com.cd_ord_com                                           AS código_ordem_compra,
+    ord_com.dt_ord_com                                           AS data_da_ordem_compra,
+    cotador.ds_cotador                                           AS comprador,
+    (
+        SELECT
+            autorizador_ordem_compra.cd_usuario
+        FROM
+            autorizador_ordem_compra
+        WHERE
+            cd_ord_com = ord_com.cd_ord_com
+        ORDER BY
+            dt_autorizacao DESC
+        FETCH FIRST 1 ROW ONLY
+    )                                                            AS usuario_autorizador,
+    (
+        SELECT
+            autorizador_ordem_compra.dt_autorizacao
+        FROM
+            autorizador_ordem_compra
+        WHERE
+            cd_ord_com = ord_com.cd_ord_com
+        ORDER BY
+            dt_autorizacao DESC
+        FETCH FIRST 1 ROW ONLY
+    )                                                            AS data_da_autorização,
+    itcon_pag.dt_quitacao,
+    decode(ord_com.tp_situacao, 'U', 'AUTORIZADA', 'T', 'ATENDIDA',
+           'A', 'AGUARDANDO PROXIMO NIVEL', ord_com.tp_situacao) AS situação,
+    produto.ds_produto,
+    itsol_com.qt_solic                                           AS quantidade_solicitada,
+    itsol_com.qt_atendida                                        AS quantidade_atendida,
+    produto.vl_ultima_entrada                                    AS valor_da_ultima,
+    ord_com.vl_total                                             AS valor_total_da_ordem
+FROM
+         ord_com
+    INNER JOIN estoque ON estoque.cd_estoque = ord_com.cd_estoque
+    INNER JOIN fornecedor ON fornecedor.cd_fornecedor = ord_com.cd_fornecedor
+    LEFT JOIN sol_com ON sol_com.cd_sol_com = ord_com.cd_sol_com
+    LEFT JOIN setor ON setor.cd_setor = sol_com.cd_setor
+    LEFT JOIN cotador ON cotador.cd_cotador = sol_com.cd_cotador
+    LEFT JOIN conta ON conta.cd_conta = sol_com.cd_conta
+    LEFT JOIN ent_pro ON ent_pro.cd_ord_com = ord_com.cd_ord_com
+    LEFT JOIN con_pag ON con_pag.nr_documento = ent_pro.nr_documento
+    LEFT JOIN itcon_pag ON itcon_pag.cd_con_pag = con_pag.cd_con_pag
+    LEFT JOIN itsol_com ON itsol_com.cd_sol_com = sol_com.cd_sol_com
+    LEFT JOIN produto ON produto.cd_produto = itsol_com.cd_produto
+WHERE
+    fornecedor.tp_cliente_forn IN ( 'F', 'A', 'R', 'T' )
+    AND sol_com.dt_sol_com BETWEEN TO_DATE('01/02/2026', 'DD/MM/YYYY') AND TO_DATE('19/02/2026', 'DD/MM/YYYY')
+ORDER BY
+    sol_com.cd_sol_com DESC,
+    sol_com.cd_sol_com
