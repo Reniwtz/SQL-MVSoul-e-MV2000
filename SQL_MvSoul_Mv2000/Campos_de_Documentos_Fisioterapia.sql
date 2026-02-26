a5988ec871c1ebc2ad823b536c8729b06262eb0f
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2026-02-26 14:49:45 -0300

    Update Campos_de_Documentos_Fisioterapia.sql

--- SQL_MvSoul_Mv2000/Campos_de_Documentos_Fisioterapia.sql
+++ SQL_MvSoul_Mv2000/Campos_de_Documentos_Fisioterapia.sql
@@ -287,6 +287,70 @@ GROUP BY
     nm_documento,
     dh_criacao;
 
+-- Óbitos
+SELECT
+    data,
+    SUM(total) AS total
+FROM
+    (
+        SELECT
+            trunc(atendime.dt_alta) AS data,
+            COUNT(*)                AS total
+        FROM
+            atendime
+        WHERE
+                atendime.tp_atendimento <> 'I'
+            AND atendime.sn_obito = 'S'
+            AND EXISTS (
+                SELECT
+                    1
+                FROM
+                         pw_documento_clinico
+                    JOIN pw_editor_clinico ON pw_editor_clinico.cd_documento_clinico = pw_documento_clinico.cd_documento_clinico
+                    JOIN editor_registro_campo ON editor_registro_campo.cd_registro = pw_editor_clinico.cd_editor_registro
+                    JOIN editor_campo ON editor_campo.cd_campo = editor_registro_campo.cd_campo
+                    JOIN paciente ON paciente.cd_paciente = pw_documento_clinico.cd_paciente
+                WHERE
+                        pw_documento_clinico.cd_paciente = atendime.cd_paciente
+                    AND pw_editor_clinico.cd_documento = '382'
+                    AND pw_documento_clinico.cd_objeto = '261'
+                    AND pw_documento_clinico.nm_documento LIKE '%FISIOTERAPIA%'
+            )
+        GROUP BY
+            trunc(atendime.dt_alta)
+        UNION ALL
+        SELECT
+            trunc(atendime.dt_alta) AS data,
+            COUNT(*)                AS total
+        FROM
+            atendime
+        WHERE
+            atendime.cd_mot_alt IS NOT NULL
+            AND atendime.tp_atendimento = 'I'
+            AND atendime.sn_obito = 'S'
+            AND EXISTS (
+                SELECT
+                    1
+                FROM
+                         pw_documento_clinico
+                    JOIN pw_editor_clinico ON pw_editor_clinico.cd_documento_clinico = pw_documento_clinico.cd_documento_clinico
+                    JOIN editor_registro_campo ON editor_registro_campo.cd_registro = pw_editor_clinico.cd_editor_registro
+                    JOIN editor_campo ON editor_campo.cd_campo = editor_registro_campo.cd_campo
+                    JOIN paciente ON paciente.cd_paciente = pw_documento_clinico.cd_paciente
+                WHERE
+                        pw_documento_clinico.cd_paciente = atendime.cd_paciente
+                    AND pw_editor_clinico.cd_documento = '384'
+                    AND pw_documento_clinico.cd_objeto = '261'
+                    AND pw_documento_clinico.nm_documento LIKE '%FISIOTERAPIA%'
+            )
+        GROUP BY
+            trunc(atendime.dt_alta)
+    )
+GROUP BY
+    data
+ORDER BY
+    data;
+
 
 --------------------------------------------------------------------------------------------------------
 -- Fisioterapia evolução de UTI pediatrica
@@ -352,7 +416,7 @@ FROM
 WHERE
         --pw_documento_clinico.cd_atendimento = '4285233'
         pw_documento_clinico.dh_criacao BETWEEN TO_DATE('01/02/25', 'DD/MM/YY') AND TO_DATE('31/12/25', 'DD/MM/YY')
-    AND pw_editor_clinico.cd_documento = '382'
+    AND pw_editor_clinico.cd_documento = '383'
     AND pw_documento_clinico.cd_objeto = '261'
     AND pw_documento_clinico.nm_documento LIKE '%FISIOTERAPIA%'
     --AND pw_documento_clinico.cd_usuario LIKE '%CLAUDIA.BRASILEIRO%'
@@ -407,6 +471,72 @@ GROUP BY
     cd_atendimento,
     nm_documento,
     dh_criacao;
+
+
+-- Óbitos
+SELECT
+    data,
+    SUM(total) AS total
+FROM
+    (
+        SELECT
+            trunc(atendime.dt_alta) AS data,
+            COUNT(*)                AS total
+        FROM
+            atendime
+        WHERE
+                atendime.tp_atendimento <> 'I'
+            AND atendime.sn_obito = 'S'
+            AND EXISTS (
+                SELECT
+                    1
+                FROM
+                         pw_documento_clinico
+                    JOIN pw_editor_clinico ON pw_editor_clinico.cd_documento_clinico = pw_documento_clinico.cd_documento_clinico
+                    JOIN editor_registro_campo ON editor_registro_campo.cd_registro = pw_editor_clinico.cd_editor_registro
+                    JOIN editor_campo ON editor_campo.cd_campo = editor_registro_campo.cd_campo
+                    JOIN paciente ON paciente.cd_paciente = pw_documento_clinico.cd_paciente
+                WHERE
+                        pw_documento_clinico.cd_paciente = atendime.cd_paciente
+                    AND pw_editor_clinico.cd_documento = '383'
+                    AND pw_documento_clinico.cd_objeto = '261'
+                    AND pw_documento_clinico.nm_documento LIKE '%FISIOTERAPIA%'
+            )
+        GROUP BY
+            trunc(atendime.dt_alta)
+        UNION ALL
+        SELECT
+            trunc(atendime.dt_alta) AS data,
+            COUNT(*)                AS total
+        FROM
+            atendime
+        WHERE
+            atendime.cd_mot_alt IS NOT NULL
+            AND atendime.tp_atendimento = 'I'
+            AND atendime.sn_obito = 'S'
+            AND EXISTS (
+                SELECT
+                    1
+                FROM
+                         pw_documento_clinico
+                    JOIN pw_editor_clinico ON pw_editor_clinico.cd_documento_clinico = pw_documento_clinico.cd_documento_clinico
+                    JOIN editor_registro_campo ON editor_registro_campo.cd_registro = pw_editor_clinico.cd_editor_registro
+                    JOIN editor_campo ON editor_campo.cd_campo = editor_registro_campo.cd_campo
+                    JOIN paciente ON paciente.cd_paciente = pw_documento_clinico.cd_paciente
+                WHERE
+                        pw_documento_clinico.cd_paciente = atendime.cd_paciente
+                    AND pw_editor_clinico.cd_documento = '384'
+                    AND pw_documento_clinico.cd_objeto = '261'
+                    AND pw_documento_clinico.nm_documento LIKE '%FISIOTERAPIA%'
+            )
+        GROUP BY
+            trunc(atendime.dt_alta)
+    )
+GROUP BY
+    data
+ORDER BY
+    data;
+
 
 
 --------------------------------------------------------------------------------------------------------
