320ebe32645c01080b5bc11d4deb9a571ca11766
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2026-03-23 11:28:40 -0300

    Update Compra_X_Solicitação_Paga.sql

--- SQL_MvSoul_Mv2000/Compra_X_Solicitação_Paga.sql
+++ SQL_MvSoul_Mv2000/Compra_X_Solicitação_Paga.sql
@@ -54,3 +54,68 @@ WHERE
     AND sol_com.cd_sol_com = 22007
 ORDER BY
     sol_com.cd_sol_com
+
+-----------------------------------------------------------------------
+SELECT
+    sol_com.cd_sol_com                                           AS código_da_solic_de_compra,
+    sol_com.dt_sol_com                                           AS dt_solicitação_de_compra,
+    sol_com.cd_usuario                                           AS usuário_da_solicitação,
+    setor.nm_setor                                               AS setor_solicitante,
+    ord_com.cd_ord_com                                           AS ordem_de_compra,
+    ord_com.dt_ord_com                                           AS dt_ordem_de_compra,
+    ord_com.cd_usuario_criador_oc                                AS usuário_criador_da_oc,
+    ord_com.dt_prev_entrega_intervalo                            AS previsão_de_entrega,
+    ord_com.cd_fornecedor                                        AS código_do_fornecedor,
+    fornecedor.nm_fornecedor                                     AS nome_do_fornecedor,
+    itord_pro.cd_produto                                         AS código_do_produto,
+    produto.ds_produto                                           AS descrição_do_produto,
+    est_pro.qt_estoque_atual                                     AS quantida_atual_de_estoque,
+    estoque.ds_estoque                                           AS descrição_do_estoque,
+    itord_pro.qt_comprada                                        AS quantidade_comprada,
+    itord_pro.qt_recebida                                        AS quantidade_recebida,
+    itord_pro.qt_atendida                                        AS quantidade_atendida,
+    itord_pro.vl_unitario                                        AS valor_unitário,
+    itord_pro.vl_total                                           AS valor_total_dos_item,
+    ord_com.vl_total                                             AS valor_total_da_ordem,
+    decode(ord_com.tp_situacao, 'U', 'AUTORIZADA', 'T', 'ATENDIDA',
+           'A', 'AGUARDANDO PROXIMO NIVEL', ord_com.tp_situacao) AS status_da_solic_compra,
+    (
+        SELECT
+            LISTAGG(autorizador_ordem_compra.cd_usuario
+                    || ' - '
+                    || to_char(autorizador_ordem_compra.dt_autorizacao, 'DD/MM/YYYY HH24:MI:SS'), ' | ') WITHIN GROUP(
+            ORDER BY
+                autorizador_ordem_compra.dt_autorizacao
+            )
+        FROM
+            autorizador_ordem_compra autorizador_ordem_compra
+        WHERE
+            autorizador_ordem_compra.cd_ord_com = ord_com.cd_ord_com
+    )                                                            AS autorizadores_com_datas,
+    ent_pro.cd_ent_pro                                           AS código_da_entrada_do_produto,
+    ent_pro.cd_estoque                                           AS estoque_de_entrada,
+    ent_pro.hr_entrada                                           AS data_da_entrada,
+    ent_pro.nr_documento                                         AS nota_fiscal,
+    ent_pro.vl_total                                             AS valor_total_da_nota,
+    con_pag.cd_con_pag                                           AS código_contas_a_pagar,
+    con_pag.dt_lancamento                                        AS dt_lancamentento,
+    con_pag.cd_reduzido                                          AS conta_contábil
+FROM
+         ord_com ord_com
+    INNER JOIN fornecedor ON fornecedor.cd_fornecedor = ord_com.cd_fornecedor
+    INNER JOIN itord_pro ON itord_pro.cd_ord_com = ord_com.cd_ord_com
+    INNER JOIN produto ON produto.cd_produto = itord_pro.cd_produto
+    INNER JOIN sol_com ON sol_com.cd_sol_com = ord_com.cd_sol_com
+    INNER JOIN setor ON setor.cd_setor = sol_com.cd_setor
+    LEFT JOIN ent_pro ON ent_pro.cd_ord_com = ord_com.cd_ord_com
+    LEFT JOIN con_pag ON con_pag.cd_con_pag = ent_pro.cd_con_pag
+    LEFT JOIN est_pro ON est_pro.qt_estoque_atual = produto.cd_produto
+    LEFT JOIN estoque ON estoque.cd_estoque = est_pro.cd_estoque
+WHERE
+        con_pag.cd_fornecedor = '2'
+    AND con_pag.nr_documento = '61495'
+    AND con_pag.dt_emissao = TO_DATE('30/01/26', 'DD/MM/YY')
+ORDER BY
+    sol_com.cd_sol_com,
+    ord_com.cd_ord_com,
+    ent_pro.nr_documento
