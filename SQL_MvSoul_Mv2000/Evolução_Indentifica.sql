32b6892bc6fd386452fed13d64437d5dca7e4c00
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2026-03-25 17:51:38 -0300

    Update Evolução_Indentifica.sql

--- SQL_MvSoul_Mv2000/Evolução_Indentifica.sql
+++ SQL_MvSoul_Mv2000/Evolução_Indentifica.sql
@@ -46,3 +46,74 @@ ORDER BY
     pw_documento_clinico.cd_prestador,
     prestador.nm_prestador,
     pw_documento_clinico.cd_documento_clinico
+
+--------------------------------------------------------------------------------
+WITH documentos AS (
+    SELECT
+        pw_documento_clinico.cd_paciente                                                                    AS paciente,
+        pw_documento_clinico.cd_atendimento                                                                 AS atendimento,
+        pw_documento_clinico.cd_prestador                                                                   AS codigo_do_prestador,
+        prestador.nm_prestador                                                                              AS nome_do_prestador,
+        prestador.cd_prestador_muitos                                                                       AS codigo_do_chefe,
+        pw_documento_clinico.cd_documento_clinico                                                           AS documento,
+        pw_documento_clinico.tp_status                                                                      AS status,
+        pw_documento_clinico.dh_criacao                                                                     AS dt_criacao_documento,
+        decode(pw_documento_clinico.cd_objeto, 642, 'EVOLUÇÃO TERAPEUTA OCUPACIONAL', 61, 'EVOLUÇÃO MEDICA INTERNAÇÃO',
+               62, 'EVOLUÇÃO ENFERMAGEM INTERNAÇÃO', 63, 'EVOLUÇÃO NUTRICIONISTA', 73,
+               'EVOLUÇÃO PSICOLOGIA', 87, 'EVOLUÇÃO MEDICA URGÊNCIA', 90, 'EVOLUÇÃO ENFERMAGEM AMBULATORI',
+               116, 'EVOLUÇÃO FISIO INTERNAÇAO', 67, 'EVOLUÇÃO FISIOTERAPEUTA', 76,
+               'EVOLUÇÃO ASSISTENTE SOCIAL', 78, 'EVOLUÇÃO FONOAUDIOLOGIA', 79, 'EVOLUÇÃO TECNICO DE ENFERMAGEM',
+               80, 'EVOLUÇÃO FARMACIA', 82, 'EVOLUÇÃO MEDICA AMBULATORIO', 83,
+               'EVOLUÇÃO NUTRICIONISTA AMB', 202, 'EVOLUÇÃO ENFERMAGEM URGENCIA', 881, 'EVOLUÇÃO DENTISTA',
+               921, 'EVOLUÇÃO DE ENFERMAGEM CCIH', 69, 'EVOLUÇÃO DE TÉCNICO DE ENFERMAGEM', 'DESCONHECIDO') AS descricao_observacao,
+        decode(prestador.tp_vinculo, 'C', 'COOPERADO', 'U', 'FUNCIONARIO',
+               'F', 'PESSOA FISICA', 'J', 'PESSOA JURIDICA', 'V',
+               'VOLUNTARIO', 'O', 'OUTROS', prestador.tp_vinculo)                                           AS tp_vinculo_descricao
+    FROM
+             pw_documento_clinico
+        INNER JOIN pw_tipo_documento ON pw_documento_clinico.cd_tipo_documento = pw_tipo_documento.cd_tipo_documento
+        LEFT JOIN pre_med ON pre_med.cd_documento_clinico = pw_documento_clinico.cd_documento_clinico
+        LEFT JOIN evo_enf ON evo_enf.cd_documento_clinico = pw_documento_clinico.cd_documento_clinico
+        INNER JOIN prestador ON prestador.cd_prestador = pw_documento_clinico.cd_prestador
+        INNER JOIN esp_med ON esp_med.cd_prestador = prestador.cd_prestador
+    WHERE
+        pw_documento_clinico.cd_atendimento IN (
+            SELECT DISTINCT
+                cd_atendimento
+            FROM
+                atendime
+            WHERE
+                    tp_atendimento = 'I'
+                AND cd_servico LIKE '1'
+                AND cd_convenio LIKE '1'
+          -- AND dt_atendimento BETWEEN TO_DATE('01/01/25','DD/MM/YY') AND TO_DATE('31/12/25','DD/MM/YY')
+                AND cd_paciente LIKE '393995'
+                AND cd_atendimento LIKE '4071665'
+        )
+        AND pw_documento_clinico.cd_objeto IN ( 642, 61, 62, 63, 73,
+                                                87, 90, 116, 67, 76,
+                                                78, 79, 80, 82, 83,
+                                                202, 881, 921, 69 )
+        AND esp_med.cd_especialid = 23
+)
+SELECT
+    d.paciente,
+    d.atendimento,
+    d.codigo_do_prestador,
+    d.nome_do_prestador,
+    d.codigo_do_chefe,
+    chefe.nm_prestador AS nome_do_chefe,
+    d.documento,
+    d.status,
+    d.dt_criacao_documento,
+    d.descricao_observacao,
+    d.tp_vinculo_descricao
+FROM
+    documentos d
+    LEFT JOIN prestador  chefe ON chefe.cd_prestador = d.codigo_do_chefe
+ORDER BY
+    d.paciente,
+    d.atendimento,
+    d.codigo_do_prestador,
+    d.nome_do_prestador,
+    d.documento;
