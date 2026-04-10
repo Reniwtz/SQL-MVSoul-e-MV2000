54a2b417e3062e5b7556661f916b6f4f48157171
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2026-04-10 10:38:37 -0300

    Add table privileges to user access query

--- SQL_MvSoul_Mv2000/Banco_de_Oracle.sql
+++ SQL_MvSoul_Mv2000/Banco_de_Oracle.sql
@@ -1,8 +1,10 @@
 -- Usuários de Banco e seus acesso a tabela
 SELECT
-    username,
-    created,
-    last_login
+    dba_users.username,
+    dba_users.created,
+    dba_users.last_login,
+    dba_tab_privs.table_name,
+    dba_tab_privs.privilege
 FROM
     dba_users dba_users
     inner join dba_tab_privs on dba_tab_privs.grantee = dba_users.username
