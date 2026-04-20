4a2b16cced4ff012e64634bdb6ed8c3ff8c01aa9
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2026-04-20 09:41:29 -0300

    Update Atendimento_Caixa_X_Faturamento.sql

--- SQL_MvSoul_Mv2000/Atendimento_Caixa_X_Faturamento.sql
+++ SQL_MvSoul_Mv2000/Atendimento_Caixa_X_Faturamento.sql
@@ -1,35 +1,47 @@
-SELECT
-    c.cd_atendimento AS cd_atendimento_caucao,
-    c.nm_proprietario,
-    c.dt_caucao      AS dt_caucao,
-    c.vl_caucao,
-    i.cd_atendimento AS cd_atendimento_itreg_amb,
-    i.hr_lancamento
+SELECT DISTINCT
+    contrato_adiantamento.cd_contrato_adiant,
+    contrato_adiantamento.cd_atendimento,
+    to_char(contrato_adiantamento.cd_contratante),
+    caucao.cd_caucao,
+    caucao.cd_atendimento,
+    caucao.nm_proprietario,
+    itreg_amb.cd_reg_amb,
+    itreg_amb.cd_lancamento,
+    itreg_amb.hr_lancamento,
+    itreg_amb.qt_lancamento,
+    itreg_amb.vl_unitario,
+    itreg_amb.vl_desconto,
+    itreg_amb.vl_total_conta,
+    itreg_amb.cd_convenio,
+    itreg_amb.cd_pro_fat,
+    pro_fat.ds_pro_fat,
+    itreg_amb.dt_fechamento,
+    itreg_amb.nm_usuario_fechou,
+    reg_amb.cd_remessa,
+    reccon_rec.nr_documento,
+    reccon_rec.ds_reccon_rec,
+    con_rec.cd_con_rec,
+    con_rec.tp_quitacao,
+    con_rec.vl_previsto,
+    con_rec.cd_reduzido,
+    itcon_rec.nr_parcela,
+    con_rec.vl_recebido,
+    lcto_contabil.cd_lcto_contabil,
+    lcto_contabil.dt_lcto,
+    lcto_contabil.cd_reduzido_credito,
+    lcto_contabil.cd_reduzido_debito,
+    lcto_contabil.vl_lancado
 FROM
-    (
-        SELECT
-            cd_atendimento,
-            nm_proprietario,
-            dt_caucao,
-            vl_caucao
-        FROM
-            caucao
-        WHERE
-                dt_caucao >= TO_DATE('01/12/2024', 'DD/MM/YYYY')
-            AND dt_caucao < TO_DATE('31/01/2026', 'DD/MM/YYYY') + 1
-    )        c
-    FULL OUTER JOIN (
-        SELECT DISTINCT
-            cd_atendimento,
-            hr_lancamento
-        FROM
-            itreg_amb
-    )        i ON i.cd_atendimento = c.cd_atendimento
-    INNER JOIN atendime a ON a.cd_atendimento = nvl(c.cd_atendimento, i.cd_atendimento)
+    contrato_adiantamento
+    FULL OUTER JOIN caucao ON contrato_adiantamento.cd_atendimento = caucao.cd_atendimento
+    LEFT JOIN itreg_amb ON itreg_amb.cd_atendimento = coalesce(contrato_adiantamento.cd_atendimento, caucao.cd_atendimento)
+    LEFT JOIN pro_fat ON pro_fat.cd_pro_fat = itreg_amb.cd_pro_fat
+    LEFT JOIN reg_amb ON reg_amb.cd_reg_amb = itreg_amb.cd_reg_amb
+    LEFT JOIN reccon_rec ON reccon_rec.cd_caucao = caucao.cd_caucao
+                            OR reccon_rec.cd_contrato_adiant = contrato_adiantamento.cd_contrato_adiant
+    LEFT JOIN itcon_rec ON itcon_rec.cd_itcon_rec = reccon_rec.cd_itcon_rec
+    LEFT JOIN con_rec ON con_rec.cd_con_rec = itcon_rec.cd_con_rec
+    LEFT JOIN rec_mov_con ON rec_mov_con.cd_reccon_rec = reccon_rec.cd_reccon_rec
+    LEFT JOIN lcto_contabil ON lcto_contabil.cd_lcto_movimento = con_rec.cd_lcto_movimento
 WHERE
-        a.cd_convenio = 16
-    AND a.dt_atendimento >= TO_DATE('01/12/2024', 'DD/MM/YYYY')
-    AND a.dt_atendimento < TO_DATE('31/01/2026', 'DD/MM/YYYY') + 1
-ORDER BY
-    c.dt_caucao,
-    nvl(c.cd_atendimento, i.cd_atendimento);
+        caucao.cd_atendimento like '4303395'
