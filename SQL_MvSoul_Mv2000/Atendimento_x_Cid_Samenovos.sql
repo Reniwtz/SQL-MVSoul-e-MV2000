ac619b54b22a283445b7d610415fb3ec10eab218
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2026-05-28 10:52:46 -0300

    Update Atendimento_x_Cid_Samenovos.sql

--- SQL_MvSoul_Mv2000/Atendimento_x_Cid_Samenovos.sql
+++ SQL_MvSoul_Mv2000/Atendimento_x_Cid_Samenovos.sql
@@ -90,7 +90,51 @@ contagem AS (
     )
     AND a.dt_atendimento >= TO_DATE('01/01/2025', 'DD/MM/YYYY')
     AND a.dt_atendimento <  TO_DATE('31/01/2025', 'DD/MM/YYYY') + 1
+    AND a.cd_convenio in ('1', '2')
     GROUP BY a.cd_procedimento
+)
+SELECT
+    l.cd_procedimento,
+    NVL(c.qtd, 0) AS qtd
+FROM lista l
+LEFT JOIN contagem c
+    ON c.cd_procedimento = l.cd_procedimento
+ORDER BY
+    l.ordem;
+
+
+-- Pedidos de imagem e anatomopatologico
+WITH lista AS (
+    SELECT
+        LEVEL AS ordem,
+        REGEXP_SUBSTR(codigos, '[^[:space:]]+', 1, LEVEL) AS cd_procedimento
+    FROM (
+        SELECT q'[
+0201010410
+0201010470
+0201010542
+....
+]' AS codigos
+        FROM dual
+    )
+    CONNECT BY REGEXP_SUBSTR(codigos, '[^[:space:]]+', 1, LEVEL) IS NOT NULL
+),
+contagem AS (
+    SELECT
+        e.cd_procedimento_sia AS cd_procedimento,
+        COUNT(*) AS qtd
+    FROM ped_rx p
+    INNER JOIN exa_rx e
+        ON e.cd_exa_rx = p.cd_exa_rx
+    WHERE p.dt_pedido >= TO_DATE('01/01/2025', 'DD/MM/YYYY')
+      AND p.dt_pedido <  TO_DATE('31/01/2025', 'DD/MM/YYYY') + 1
+      AND p.cd_convenio in ('1', '2')
+      AND e.cd_procedimento_sia IN (
+            SELECT cd_procedimento
+            FROM lista
+      )
+    GROUP BY
+        e.cd_procedimento_sia
 )
 SELECT
     l.cd_procedimento,
